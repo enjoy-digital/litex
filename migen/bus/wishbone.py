c43f3da534ab258767815b53cd40ce3bf514ff63
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-12-08 18:47:41 +0100

    Wishbone declarations

---
+++ migen/bus/wishbone.py
@@ -0,0 +1,23 @@
+from migen.fhdl import structure as f
+from .simple import Simple
+
+_desc = [
+	(True,	"adr",	32),
+	(True,	"dat",	32),
+	(False,	"dat",	32),
+	(True,	"cyc",	1),
+	(True,	"stb",	1),
+	(False,	"ack",	1),
+	(True,	"we",	1),
+	(True,	"cti",	3),
+	(True,	"bte",	2),
+	(False,	"err",	1)
+]
+
+class Master(Simple):
+	def __init__(self):
+		Simple.__init__(self, _desc, False)
+
+class Slave(Simple):
+	def __init__(self):
+		Simple.__init__(self, _desc, True)
