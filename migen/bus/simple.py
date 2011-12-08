c1041b9a5f3c38d1d1a3f8c46c54428153dcffdb
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-12-08 23:06:04 +0100

    simplebus: export GetSigName function

--- migen/bus/simple.py
+++ migen/bus/simple.py
@@ -1,5 +1,12 @@
 from migen.fhdl import structure as f
 
+def GetSigName(signal, slave):
+	if signal[0] ^ slave:
+		suffix = "_o"
+	else:
+		suffix = "_i"
+	return signal[1] + suffix
+
 # desc is a list of tuples, each made up of:
 # 0) boolean: "master to slave"
 # 1) string: name
@@ -7,13 +14,9 @@ from migen.fhdl import structure as f
 class Simple():
 	def __init__(self, desc, slave, name):
 		for signal in desc:
-			if signal[0] ^ slave:
-				suffix = "_o"
-			else:
-				suffix = "_i"
 			modules = self.__module__.split('.')
 			busname = modules[len(modules)-1]
 			if name:
 				busname += "_" + name
-			signame = signal[1]+suffix
+			signame = GetSigName(signal, slave)
 			setattr(self, signame, f.Signal(f.BV(signal[2]), busname + "_" + signame))
