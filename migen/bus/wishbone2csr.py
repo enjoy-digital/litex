929cc9807020ebaba3c1a6a77e786f5a8160c962
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-12-13 17:38:59 +0100

    wishbone2csr: wait for WB deack

--- migen/bus/wishbone2csr.py
+++ migen/bus/wishbone2csr.py
@@ -9,14 +9,14 @@ class Inst():
 		self.csr = csr.Master("from_wishbone")
 		self.timeline = timeline.Inst(self.wishbone.cyc_i & self.wishbone.stb_i,
 			[(1, [f.Assign(self.csr.we_o, self.wishbone.we_i)]),
-			(2, [f.Assign(self.wishbone.ack_o, 1)])])
+			(2, [f.Assign(self.wishbone.ack_o, 1)]),
+			(3, [f.Assign(self.wishbone.ack_o, 0)])])
 	
 	def GetFragment(self):
 		sync = [
 			f.Assign(self.csr.we_o, 0),
 			f.Assign(self.csr.d_o, self.wishbone.dat_i),
 			f.Assign(self.csr.a_o, self.wishbone.adr_i[2:16]),
-			f.Assign(self.wishbone.ack_o, 0),
 			f.Assign(self.wishbone.dat_o, self.csr.d_i)
 		]
 		return f.Fragment(sync=sync) + self.timeline.GetFragment()
