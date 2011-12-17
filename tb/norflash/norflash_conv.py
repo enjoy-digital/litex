1b3edd07ca010c68b42f2934b203993ed5897be9
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-12-17 15:22:26 +0100

    norflash tb: use get_fragment

--- tb/norflash/norflash_conv.py
+++ tb/norflash/norflash_conv.py
@@ -1,9 +1,10 @@
 from migen.fhdl import verilog
 from migen.bus import wishbone
+
 from milkymist import norflash
 
 norflash0 = norflash.Inst(25, 12)
-frag = norflash0.GetFragment()
+frag = norflash0.get_fragment()
 v = verilog.Convert(frag, name="norflash",
 	ios={norflash0.bus.cyc_i, norflash0.bus.stb_i, norflash0.bus.we_i, norflash0.bus.adr_i, norflash0.bus.sel_i, norflash0.bus.dat_i, norflash0.bus.dat_o, norflash0.bus.ack_o})
 print(v)
