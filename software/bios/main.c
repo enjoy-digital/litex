ef0667d95962aaf8e9267e9a9712d0ead97994cd
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-02-07 14:12:33 +0100

    software: UART RX demo

--- software/bios/main.c
+++ software/bios/main.c
@@ -4,11 +4,16 @@
 
 int main(void)
 {
+	char c;
+	
 	irq_setmask(0);
 	irq_setie(1);
 	uart_init();
 	
 	printf("Hello World with IRQs\n");
 	
-	while(1);
+	while(1) {
+		c = uart_read();
+		printf("You typed: %c\n", c);
+	}
 }
