59db4e9106300dde8025bc92d0dbd184fd44e495
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-03-09 17:16:33 +0100

    doc: add logo

--- doc/conf.py
+++ doc/conf.py
@@ -184,7 +184,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = "migen_logo.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
