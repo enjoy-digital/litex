22f7d1716e18e8ca6a575cd13ef6fd8d432445cb
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-05-24 19:22:27 +0200

    Remove some boilerplate

--- software/bios/microudp.h
+++ software/bios/microudp.h
@@ -1,22 +1,5 @@
-/*
- * Milkymist SoC (Software)
- * Copyright (C) 2007, 2008, 2009, 2010, 2011 Sebastien Bourdeauducq
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#ifndef __NET_MICROUDP_H
-#define __NET_MICROUDP_H
+#ifndef __MICROUDP_H
+#define __MICROUDP_H
 
 #define IPTOINT(a, b, c, d) ((a << 24)|(b << 16)|(c << 8)|d)
 
@@ -31,4 +14,4 @@ int microudp_send(unsigned short src_port, unsigned short dst_port, unsigned int
 void microudp_set_callback(udp_callback callback);
 void microudp_service(void);
 
-#endif /* __NET_MICROUDP_H */
+#endif /* __MICROUDP_H */
--- verilog/minimac3/minimac3_tx.v
+++ verilog/minimac3/minimac3_tx.v
@@ -1,20 +1,3 @@
-/*
- * Milkymist SoC
- * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012 Sebastien Bourdeauducq
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 module minimac3_tx(
 	input phy_tx_clk,
 
