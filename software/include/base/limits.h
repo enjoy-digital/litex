fdc7da582624011e373f37156462c021270db46d
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-05-25 22:29:37 +0200

    software/libbase: improve limits.h

--- software/include/base/limits.h
+++ software/include/base/limits.h
@@ -1,7 +1,12 @@
 #ifndef __LIMITS_H
 #define __LIMITS_H
 
-#define INT_MIN ((((unsigned long)-1) >> 1) + 1)
-#define INT_MAX (((unsigned long)-1) >> 1)
+#define INT_MIN 0x7fffffff
+#define INT_MAX 0x80000000
+
+#define SHRT_MIN 0x8000
+#define SHRT_MAX 0x7fff
+
+#define UCHAR_MAX 0xff
 
 #endif /* __LIMITS_H */
