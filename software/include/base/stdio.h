56e201fd5d40a128a4703b9843dea27c86ca86bc
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-05-28 17:17:13 +0200

    software/libbase: more file decls in stdio

--- software/include/base/stdio.h
+++ software/include/base/stdio.h
@@ -21,6 +21,10 @@ int printf(const char *fmt, ...);
 #define BUFSIZ 1024
 #endif
 
+#ifndef EOF
+#define EOF -1
+#endif
+
 typedef int FILE;
 
 extern FILE *stdin;
@@ -31,9 +35,14 @@ int fprintf(FILE *stream, const char *format, ...);
 int fflush(FILE *stream);
 
 FILE *fopen(const char *path, const char *mode);
+FILE *freopen(const char *path, const char *mode, FILE *stream);
 char *fgets(char *s, int size, FILE *stream);
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+int getc(FILE *stream);
+int fputc(int c, FILE *stream);
+int ferror(FILE *stream);
+int feof(FILE *stream);
 int fclose(FILE *fp);
 
 #endif /* __STDIO_H */
