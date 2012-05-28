e115eb08fb76eb845586d322e7b248bc6c256be1
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-05-28 19:32:38 +0200

    software/libbase: fix stupid mistake in limits.h

--- software/include/base/limits.h
+++ software/include/base/limits.h
@@ -1,8 +1,8 @@
 #ifndef __LIMITS_H
 #define __LIMITS_H
 
-#define INT_MIN 0x7fffffff
-#define INT_MAX 0x80000000
+#define INT_MIN 0x80000000
+#define INT_MAX 0x7fffffff
 
 #define SHRT_MIN 0x8000
 #define SHRT_MAX 0x7fff
