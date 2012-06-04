b4f49cc48fec2fb0985e6df03873b9a4b01c79db
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-06-04 19:41:41 +0200

    software/base/limits.h: add some more

--- software/include/base/limits.h
+++ software/include/base/limits.h
@@ -1,12 +1,18 @@
 #ifndef __LIMITS_H
 #define __LIMITS_H
 
+#define ULONG_MAX 0xffffffff
+
+#define UINT_MAX 0xffffffff
 #define INT_MIN 0x80000000
 #define INT_MAX 0x7fffffff
 
+#define USHRT_MAX 0xffff
 #define SHRT_MIN 0x8000
 #define SHRT_MAX 0x7fff
 
 #define UCHAR_MAX 0xff
+
+#define CHAR_BIT 8
 
 #endif /* __LIMITS_H */
