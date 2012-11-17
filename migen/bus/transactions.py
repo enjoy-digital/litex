d0d4c48098e4f78eeee6417314ea144957d88aa3
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-11-17 19:36:08 +0100

    bus/transactions: add busname parameter

--- migen/bus/transactions.py
+++ migen/bus/transactions.py
@@ -1,13 +1,14 @@
 from migen.fhdl.structure import bits_for
 
 class Transaction:
-	def __init__(self, address, data=0, sel=None):
+	def __init__(self, address, data=0, sel=None, busname=None):
 		self.address = address
 		self.data = data
 		if sel is None:
 			bytes = (bits_for(data) + 7)//8
 			sel = 2**bytes - 1
 		self.sel = sel
+		self.busname = busname
 		self.latency = 0
 	
 	def __str__(self):
