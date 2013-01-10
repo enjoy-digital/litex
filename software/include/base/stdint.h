b0503aaf852460fd467bb84129273a028270f069
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-01-10 17:10:29 +0100

    software/include/base/stdint.h: more definitions

--- software/include/base/stdint.h
+++ software/include/base/stdint.h
@@ -3,5 +3,21 @@
 
 typedef unsigned int uintptr_t;
 
-#endif /* __STDINT_H */
+typedef unsigned long long uint64_t;
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
 
+typedef long long int64_t;
+typedef int int32_t;
+typedef short int16_t;
+typedef char int8_t;
+
+#define __int_c_join(a, b) a ## b
+#define __int_c(v, suffix) __int_c_join(v, suffix)
+#define __uint_c(v, suffix) __int_c_join(v##U, suffix)
+
+#define INT64_C(v) __int_c(v, LL)
+#define UINT64_C(v) __uint_c(v, LL)
+
+#endif /* __STDINT_H */
