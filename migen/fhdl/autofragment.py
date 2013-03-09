d0d2df3c4b4f7e3ed5718d900d7444623379578d
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-03-09 23:05:45 +0100

    fhdl/autofragment: remove legacy functions

--- migen/fhdl/autofragment.py
+++ migen/fhdl/autofragment.py
@@ -1,24 +1,5 @@
-import inspect
-
 from migen.fhdl.structure import *
 from migen.fhdl.specials import Special
-
-def from_local():
-	f = Fragment()
-	frame = inspect.currentframe().f_back
-	ns = frame.f_locals
-	for x in ns:
-		obj = ns[x]
-		if hasattr(obj, "get_fragment"):
-			f += obj.get_fragment()
-	return f
-
-def from_attributes(obj):
-	f = Fragment()
-	for x in obj.__dict__.values():
-		if hasattr(x, "get_fragment"):
-			f += x.get_fragment()
-	return f
 
 class _FModuleProxy:
 	def __init__(self, fm):
