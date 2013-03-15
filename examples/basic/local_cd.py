208e039bbbd854e63c4632b2085b96e8b99145f0
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-03-15 18:18:32 +0100

    Local clock domain example

---
+++ examples/basic/local_cd.py
@@ -0,0 +1,17 @@
+from migen.fhdl.structure import *
+from migen.fhdl.module import Module
+from migen.fhdl import verilog
+from migen.genlib.divider import Divider
+
+class CDM(Module):
+	def __init__(self):
+		self.submodules.divider = Divider(5)
+		self.clock_domains.cd_sys = ClockDomain()
+
+class MultiMod(Module):
+	def __init__(self):
+		self.submodules.foo = CDM()
+		self.submodules.bar = CDM()
+
+mm = MultiMod()
+print(verilog.convert(mm, {mm.foo.cd_sys.clk, mm.bar.cd_sys.clk}))
