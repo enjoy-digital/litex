af4eb02551300434d676980d9877da428ad63adf
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-03-18 18:37:23 +0100

    examples/basic/arrays: demonstrate lowering of Array in Instance expression

--- examples/basic/arrays.py
+++ examples/basic/arrays.py
@@ -1,11 +1,12 @@
 from migen.fhdl.structure import *
+from migen.fhdl.specials import Instance
 from migen.fhdl.module import Module
 from migen.fhdl import verilog
 
 class Example(Module):
 	def __init__(self):
-		dx = 5
-		dy = 5
+		dx = 2
+		dy = 2
 
 		x = Signal(max=dx)
 		y = Signal(max=dy)
@@ -19,5 +20,9 @@ class Example(Module):
 		self.sync += If(we,
 				my_2d_array[x][y].eq(inp)
 			)
+
+		ina = Array(Signal() for a in range(dx))
+		outa = Array(Signal() for a in range(dy))
+		self.specials += Instance("test", Instance.Output("O", outa[y]), Instance.Input("I", ina[x]))
 
 print(verilog.convert(Example()))
