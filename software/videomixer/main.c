60103083170c6631bf376b21283802b242ff890f
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-03-24 00:46:23 +0100

    software/videomixer: report char position + detected resolution, detect phase at beginning

--- software/videomixer/main.c
+++ software/videomixer/main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <irq.h>
 #include <uart.h>
 #include <hw/dvisampler.h>
 
 static int d0, d1, d2;
+
+static void print_status(void)
+{
+	printf("Ph: %4d %4d %4d // %d%d%d [%d %d %d] // %d // %dx%d // %d\n", d0, d1, d2,
+		CSR_DVISAMPLER0_D0_CHAR_SYNCED,
+		CSR_DVISAMPLER0_D1_CHAR_SYNCED,
+		CSR_DVISAMPLER0_D2_CHAR_SYNCED,
+		CSR_DVISAMPLER0_D0_CTL_POS,
+		CSR_DVISAMPLER0_D1_CTL_POS,
+		CSR_DVISAMPLER0_D2_CTL_POS,
+		CSR_DVISAMPLER0_CHAN_SYNCED,
+		(CSR_DVISAMPLER0_HRESH << 8) | CSR_DVISAMPLER0_HRESL,
+		(CSR_DVISAMPLER0_VRESH << 8) | CSR_DVISAMPLER0_VRESL,
+		(CSR_DVISAMPLER0_DECNT2 << 16) | (CSR_DVISAMPLER0_DECNT1 << 8) |  CSR_DVISAMPLER0_DECNT0);
+}
 
 static void calibrate_delays(void)
 {
@@ -60,28 +76,46 @@ static void adjust_phase(void)
 			CSR_DVISAMPLER0_D2_PHASE_RESET = 1;
 			break;
 	}
-	printf("Ph: %4d %4d %4d // %d%d%d // %d\n", d0, d1, d2,
-		CSR_DVISAMPLER0_D0_CHAR_SYNCED,
-		CSR_DVISAMPLER0_D1_CHAR_SYNCED,
-		CSR_DVISAMPLER0_D2_CHAR_SYNCED,
-		CSR_DVISAMPLER0_CHAN_SYNCED);
+}
+
+static int init_phase(void)
+{
+	int od0, od1, od2; 
+	int i, j;
+
+	for(i=0;i<100;i++) {
+		od0 = d0;
+		od1 = d1;
+		od2 = d2;
+		for(j=0;j<1000;j++)
+			adjust_phase();
+		if((abs(d0 - od0) < 4) && (abs(d1 - od1) < 4) && (abs(d2 - od2) < 4))
+			return 1;
+	}
+	return 0;
 }
 
 static void vmix(void)
 {
+	int i;
 	unsigned int counter;
 
 	while(1) {
 		while(!CSR_DVISAMPLER0_PLL_LOCKED);
 		printf("PLL locked\n");
 		calibrate_delays();
-		adjust_phase();
+		if(init_phase())
+			printf("Phase init OK\n");
+		else
+			printf("Phase did not settle\n");
+		print_status();
 
 		counter = 0;
 		while(CSR_DVISAMPLER0_PLL_LOCKED) {
 			counter++;
-			if(counter == 200000) {
-				adjust_phase();
+			if(counter == 2000000) {
+				print_status();
+				//adjust_phase();
 				counter = 0;
 			}
 		}
