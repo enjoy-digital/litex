38e92eb92b28bd0605a6bd52c8ccf353a5729ef6
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-03-26 23:05:46 +0100

    altera_quartus: fix clock domain name

--- mibuild/altera_quartus.py
+++ mibuild/altera_quartus.py
@@ -14,7 +14,7 @@ def _add_period_constraint(platform, clk, period):
 class CRG_SE(SimpleCRG):
 	def __init__(self, platform, clk_name, rst_name, period, rst_invert=False):
 		SimpleCRG.__init__(self, platform, clk_name, rst_name, rst_invert)
-		_add_period_constraint(platform, self.cd.clk, period)
+		_add_period_constraint(platform, self.cd_sys.clk, period)
 
 def _format_constraint(c):
 	if isinstance(c, Pins):
