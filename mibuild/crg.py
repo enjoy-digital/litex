715d332c3d76b89053e15259accc4c4ae979e51b
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-04-08 20:28:11 +0200

    crg: apply constraint to IO pins, not internal signals

--- mibuild/crg.py
+++ mibuild/crg.py
@@ -3,9 +3,11 @@ from migen.fhdl.module import Module
 
 class SimpleCRG(Module):
 	def __init__(self, platform, clk_name, rst_name, rst_invert=False):
+			self._clk = platform.request(clk_name)
+			self._rst = platform.request(rst_name)
 			self.clock_domains.cd_sys = ClockDomain()
-			self.comb += self.cd_sys.clk.eq(platform.request(clk_name))
+			self.comb += self.cd_sys.clk.eq(self._clk)
 			if rst_invert:
-				self.comb += self.cd_sys.rst.eq(~platform.request(rst_name))
+				self.comb += self.cd_sys.rst.eq(~self._rst)
 			else:
-				self.comb += self.cd_sys.rst.eq(platform.request(rst_name))
+				self.comb += self.cd_sys.rst.eq(self._rst)
