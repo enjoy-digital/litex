08b09fb02a7b2b785c7f0a8799c1861c9db2e9b4
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-11-24 23:51:29 +0100

    do not attempt to build SDRAM PHY header when SDRAM is not present

--- make.py
+++ make.py
@@ -64,8 +64,9 @@ def main():
 		write_to_file("software/include/generated/regions.ld", boilerplate + linker_header)
 		csr_header = cpuif.get_csr_header(soc.csr_base, soc.csrbankarray, soc.interrupt_map)
 		write_to_file("software/include/generated/csr.h", boilerplate + csr_header)
-		sdram_phy_header = initsequence.get_sdram_phy_header(soc.ddrphy)
-		write_to_file("software/include/generated/sdram_phy.h", boilerplate + sdram_phy_header)
+		if hasattr(soc, "ddrphy"):
+			sdram_phy_header = initsequence.get_sdram_phy_header(soc.ddrphy)
+			write_to_file("software/include/generated/sdram_phy.h", boilerplate + sdram_phy_header)
 	if args.csr_csv:
 		csr_csv = cpuif.get_csr_csv(soc.csr_base, soc.csrbankarray)
 		write_to_file(args.csr_csv, csr_csv)
