f55943ae18b9bbc6504e0ddd3612de824916876d
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-02-15 14:01:50 +0100

    new action syntax for make.py + support xc3sprog

--- make.py
+++ make.py
@@ -8,10 +8,30 @@ from migen.fhdl import simplify
 
 from misoclib.gensoc import cpuif
 from misoclib.s6ddrphy import initsequence
-import jtag
+import programmer
 
 def _get_args():
-	parser = argparse.ArgumentParser(description="MiSoC - a high performance and small footprint SoC based on Migen.")
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+		description="""\
+MiSoC - a high performance and small footprint SoC based on Migen.
+
+This program builds and/or loads MiSoC components.
+One or several actions can be specified:
+
+build-bitstream build FPGA bitstream. Implies build-bios on targets with
+                integrated BIOS.
+build-headers   build software header files with CSR/IRQ/SDRAM_PHY definitions.
+build-csr-csv   save CSR map into CSV file.
+build-bios      build BIOS. Implies build-header.
+
+load-bitstream  load bitstream into volatile storage.
+flash-bitstream load bitstream into non-volatile storage.
+flash-bios      load BIOS into non-volatile storage.
+
+all             build-bitstream, build-bios, flash-bitstream, flash-bios.
+
+Load/flash actions use the existing outputs, and do not trigger new builds.
+""")
 
 	parser.add_argument("-p", "--platform", default="mixxeo", help="platform to build for")
 	parser.add_argument("-t", "--target", default="mlabs_video", help="SoC type to build")
@@ -22,12 +42,8 @@ def _get_args():
 
 	parser.add_argument("-d", "--decorate", default=[], action="append", help="apply simplification decorator to top-level")
 	parser.add_argument("-Ob", "--build-option", default=[], nargs=2, action="append", help="set build option")
-	parser.add_argument("-B", "--no-bitstream", default=False, action="store_true", help="do not build bitstream file")
-	parser.add_argument("-H", "--no-header", default=False, action="store_true", help="do not build C header files with CSR/IRQ/SDRAM_PHY definitions")
-	parser.add_argument("-c", "--csr-csv", default="", help="save CSR map into CSV file")
 
-	parser.add_argument("-l", "--load", default=False, action="store_true", help="load bitstream to FPGA volatile memory")
-	parser.add_argument("-f", "--flash", default=False, action="store_true", help="load bitstream to flash")
+	parser.add_argument("action", nargs="+", help="specify an action")
 
 	return parser.parse_args()
 
@@ -44,9 +60,10 @@ def _misoc_import(default, external, name):
 	else:
 		return importlib.import_module(default + "." + name)
 
-def main():
+if __name__ == "__main__":
 	args = _get_args()
 
+	# create top-level SoC object
 	platform_module = _misoc_import("mibuild.platforms", args.external_platform, args.platform)
 	target_module = _misoc_import("targets", args.external_target, args.target)
 	platform = platform_module.Platform()
@@ -57,10 +74,33 @@ def main():
 	build_name = top_class.__name__.lower() + "-" + args.platform
 	top_kwargs = dict((k, autotype(v)) for k, v in args.target_option)
 	soc = top_class(platform, **top_kwargs)
-
 	soc.finalize()
 
-	if not args.no_header:
+	# decode actions
+	action_list = ["build-bitstream", "build-headers", "build-csr-csv", "build-bios",
+		"load-bitstream", "flash-bitstream", "flash-bios", "all"]
+	actions = {k: False for k in action_list}
+	for action in args.action:
+		if action in actions:
+			actions[action] = True
+		else:
+			print("Unknown action: "+action+". Valid actions are:")
+			for a in action_list:
+				print("  "+a)
+			sys.exit(1)
+
+	# dependencies
+	if actions["all"]:
+		actions["build-bitstream"] = True
+		actions["build-bios"] = True
+		actions["flash-bitstream"] = True
+		actions["flash-bios"] = True
+	if actions["build-bitstream"] and hasattr(soc, "init_bios_memory"):
+		actions["build-bios"] = True
+	if actions["build-bios"]:
+		actions["build-headers"] = True
+
+	if actions["build-headers"]:
 		boilerplate = """/*
  * Platform: {}
  * Target: {}
@@ -75,38 +115,44 @@ def main():
 		if hasattr(soc, "ddrphy"):
 			sdram_phy_header = initsequence.get_sdram_phy_header(soc.ddrphy)
 			write_to_file("software/include/generated/sdram_phy.h", boilerplate + sdram_phy_header)
-	if args.csr_csv:
+
+	if actions["build-csr-csv"]:
 		csr_csv = cpuif.get_csr_csv(soc.csr_base, soc.csrbankarray)
 		write_to_file(args.csr_csv, csr_csv)
 
-	if hasattr(soc, "init_bios_memory"):
+	if actions["build-bios"]:
 		ret = subprocess.call(["make", "-C", "software/bios"])
 		if ret:
 			raise OSError("BIOS build failed")
-		bios_file = open("software/bios/bios.bin", "rb")
-		bios_data = []
-		while True:
-			w = bios_file.read(4)
-			if not w:
-				break
-			bios_data.append(struct.unpack(">I", w)[0])
-		bios_file.close()
+
+	if hasattr(soc, "init_bios_memory"):
+		with open("software/bios/bios.bin", "rb") as bios_file:
+			bios_data = []
+			while True:
+				w = bios_file.read(4)
+				if not w:
+					break
+				bios_data.append(struct.unpack(">I", w)[0])
 		soc.init_bios_memory(bios_data)
 
-	for decorator in args.decorate:
-		soc = getattr(simplify, decorator)(soc)
-
-	if not args.no_bitstream:
+	if actions["build-bitstream"]:
+		for decorator in args.decorate:
+			soc = getattr(simplify, decorator)(soc)
 		build_kwargs = dict((k, autotype(v)) for k, v in args.build_option)
 		platform.build(soc, build_name=build_name, **build_kwargs)
-		subprocess.call(["tools/byteswap",
-			"build/" + build_name + ".bin",
-			"build/" + build_name + ".fpg"])
 
-	if args.load:
-		jtag.load(platform.name, "build/" + build_name + ".bit")
-	if args.flash:
-		jtag.flash("build/" + build_name + ".fpg")
-
-if __name__ == "__main__":
-	main()
+	if actions["load-bitstream"] or actions["flash-bitstream"] or actions["flash-bios"]:
+		prog = programmer.create_programmer(platform.name, None)
+		if actions["load-bitstream"]:
+			prog.load_bitstream("build/" + build_name + ".bit")
+		if actions["flash-bitstream"]:
+			if prog.needs_bitreverse:
+				flashbit = "build/" + build_name + ".fpg"
+				subprocess.call(["tools/byteswap",
+					"build/" + build_name + ".bin",
+					flashbit])
+			else:
+				flashbit = "build/" + build_name + ".bin"
+			prog.flash(0, flashbit)
+		if actions["flash-bios"]:
+			prog.flash(soc.cpu_reset_address, "software/bios/bios.bin")
