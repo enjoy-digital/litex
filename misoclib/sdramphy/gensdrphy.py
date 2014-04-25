3ab9f234d06d36216d2b4f0d012e180aa0d926ed
Robert Jordens <jordens@gmail.com>
2014-04-24 18:29:36 -0600

    gensdrphy: use 'dm' not 'dqm' (follow s6ddrphy and majority of platforms)

--- misoclib/sdramphy/gensdrphy.py
+++ misoclib/sdramphy/gensdrphy.py
@@ -80,7 +80,7 @@ class GENSDRPHY(Module):
 		drive_dq = Signal()
 		self.sync += sd_dq_out.eq(self.dfi.p0.wrdata),
 		self.specials += Tristate(pads.dq, sd_dq_out, drive_dq)
-		self.comb += pads.dqm.eq(0)
+		self.comb += pads.dm.eq(0)
 		sd_dq_in_ps = Signal(d)
 		self.sync.sys_ps += sd_dq_in_ps.eq(pads.dq)
 		self.sync += self.dfi.p0.rddata.eq(sd_dq_in_ps)
