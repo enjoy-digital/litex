13e74b8b4f7ff1dcefa3ad595229594f3c23642c
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-05-14 15:01:38 +0200

    software: factorize exception_handler

--- software/bios/isr.c
+++ software/bios/isr.c
@@ -12,17 +12,3 @@ void isr(void)
 	if(irqs & (1 << UART_INTERRUPT))
 		uart_isr();
 }
-
-#ifdef __or1k__
-#define EXTERNAL_IRQ 0x800
-void exception_handler(unsigned long vect, unsigned long *sp);
-void exception_handler(unsigned long vect, unsigned long *sp)
-{
-	if ((vect & 0xf00) == EXTERNAL_IRQ) {
-		isr();
-	} else {
-		/* Unhandled exception */
-		for(;;);
-	}
-}
-#endif
---
+++ software/libbase/exception.c
@@ -0,0 +1,17 @@
+void isr(void);
+
+#ifdef __or1k__
+
+#define EXTERNAL_IRQ 0x800
+
+void exception_handler(unsigned long vect, unsigned long *sp);
+void exception_handler(unsigned long vect, unsigned long *sp)
+{
+	if ((vect & 0xf00) == EXTERNAL_IRQ) {
+		isr();
+	} else {
+		/* Unhandled exception */
+		for(;;);
+	}
+}
+#endif
