fe1c4535d0796a8a6cf9a42ac59119dd002db962
Robert Jordens <jordens@gmail.com>
2014-07-27 19:30:08 -0600

    mibuild.xilinx_vivado: support settingsXX.sh
    * in the process refactor the version search, the architecture bit width
     detection, the settings search and all also for xilinx_ise
    * use distutils.version.StrictVersion

--- mibuild/tools.py
+++ mibuild/tools.py
@@ -1,4 +1,5 @@
-import os
+import os, struct
+from distutils.version import StrictVersion
 
 def mkdir_noerror(d):
 	try:
@@ -21,3 +22,16 @@ def write_to_file(filename, contents, force_unix=False):
 	f = open(filename, "w", newline=newline)
 	f.write(contents)
 	f.close()
+
+def arch_bits():
+	return struct.calcsize("P")*8
+
+def versions(path):
+	for n in os.listdir(path):
+		full = os.path.join(path, n)
+		if not os.path.isdir(full):
+			continue
+		try:
+			yield StrictVersion(n)
+		except ValueError:
+			continue
