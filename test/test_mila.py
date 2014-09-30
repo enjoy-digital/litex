cf084fd079b83f16b4d75011cdf84e9bf897bdf4
Florent Kermarrec <florent@enjoy-digital.fr>
2014-09-30 09:29:15 +0200

    test to visualize OOB with Miscope

---
+++ test/test_mila.py
@@ -0,0 +1,20 @@
+from config import *
+from miscope.host.drivers import MiLaDriver
+
+mila = MiLaDriver(wb.regs, "mila", use_rle=True)
+wb.open()
+###
+trigger0 = mila.sataphy_host_gtx_txcominit0_o
+mask0 = mila.sataphy_host_gtx_txcominit0_m
+
+mila.prog_term(port=0, trigger=trigger0, mask=mask0)
+mila.prog_sum("term")
+
+# Trigger / wait / receive
+mila.trigger(offset=8, length=64)
+mila.wait_done()
+mila.read()
+mila.export("dump.vcd")
+mila.export("dump.csv")
+###
+wb.close()
