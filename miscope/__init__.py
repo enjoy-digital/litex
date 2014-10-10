027ddc65ca5a9aff9ddebabcfea93ccf6198f944
Florent Kermarrec <florent@enjoy-digital.fr>
2014-10-10 16:57:18 +0200

    fill __init__.py to simplify imports

---
+++ miscope/__init__.py
@@ -0,0 +1,7 @@
+try:
+	from miscope.miio import MiIo
+	from miscope.mila import MiLa
+	from miscope.trigger import Term, RangeDetector, EdgeDetector
+	from miscope.uart2wishbone import UART2Wishbone
+except:
+	pass
