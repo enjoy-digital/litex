b7028848b217751182702743bb445588e6503e5a
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-11-20 22:32:32 -0800

    ethmac: use new EndpointDescription API

--- misoclib/ethmac/common.py
+++ misoclib/ethmac/common.py
@@ -6,12 +6,9 @@ eth_preamble = 0xD555555555555555
 buffer_depth = 2**log2_int(eth_mtu, need_pow2=False)
 
 def eth_description(dw):
-	parameters = {
-		"packetized": True
-	}
 	layout = [
 		("d", dw),
 		("last_be", dw//8),
 		("error", dw//8)
 	]
-	return EndpointDescription(layout, parameters)
+	return EndpointDescription(layout, packetized=True)
