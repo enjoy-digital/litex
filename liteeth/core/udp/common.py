01d980b0622dbf9d10a06331a68f94889c667551
Florent Kermarrec <florent@enjoy-digital.fr>
2015-02-10 15:22:06 +0100

    move more things to common files

--- liteeth/core/udp/common.py
+++ liteeth/core/udp/common.py
@@ -1,4 +1,23 @@
 from liteeth.common import *
+from liteeth.generic.depacketizer import LiteEthDepacketizer
+from liteeth.generic.packetizer import LiteEthPacketizer
+from liteeth.generic.crossbar import LiteEthCrossbar
+
+class LiteEthUDPDepacketizer(LiteEthDepacketizer):
+	def __init__(self):
+		LiteEthDepacketizer.__init__(self,
+			eth_ipv4_user_description(8),
+			eth_udp_description(8),
+			udp_header,
+			udp_header_len)
+
+class LiteEthUDPPacketizer(LiteEthPacketizer):
+	def __init__(self):
+		LiteEthPacketizer.__init__(self,
+			eth_udp_description(8),
+			eth_ipv4_user_description(8),
+			udp_header,
+			udp_header_len)
 
 class LiteEthUDPMasterPort:
 	def __init__(self, dw):
@@ -27,3 +46,30 @@ class LiteEthUDPSlavePort:
 class LiteEthUDPUserPort(LiteEthUDPSlavePort):
 	def __init__(self, dw):
 		LiteEthUDPSlavePort.__init__(self, dw)
+
+class LiteEthUDPCrossbar(LiteEthCrossbar):
+	def __init__(self):
+		LiteEthCrossbar.__init__(self, LiteEthUDPMasterPort, "dst_port")
+
+	def get_port(self, udp_port, dw=8):
+		if udp_port in self.users.keys():
+			raise ValueError("Port {0:#x} already assigned".format(udp_port))
+		user_port = LiteEthUDPUserPort(dw)
+		internal_port = LiteEthUDPUserPort(8)
+		if dw != 8:
+			converter = Converter(eth_udp_user_description(user_port.dw), eth_udp_user_description(8))
+			self.submodules += converter
+			self.comb += [
+				Record.connect(user_port.sink, converter.sink),
+				Record.connect(converter.source, internal_port.sink)
+			]
+			converter = Converter(eth_udp_user_description(8), eth_udp_user_description(user_port.dw))
+			self.submodules += converter
+			self.comb += [
+				Record.connect(internal_port.source, converter.sink),
+				Record.connect(converter.source, user_port.source)
+			]
+			self.users[udp_port] = internal_port
+		else:
+			self.users[udp_port] = user_port
+		return user_port
--- liteeth/mac/__init__.py
+++ liteeth/mac/__init__.py
@@ -1,37 +1,7 @@
 from liteeth.common import *
-from liteeth.generic.depacketizer import LiteEthDepacketizer
-from liteeth.generic.packetizer import LiteEthPacketizer
-from liteeth.generic.crossbar import LiteEthCrossbar
+from liteeth.mac.common import *
 from liteeth.mac.core import LiteEthMACCore
-from liteeth.mac.frontend.common import *
 from liteeth.mac.frontend.wishbone import LiteEthMACWishboneInterface
-
-class LiteEthMACDepacketizer(LiteEthDepacketizer):
-	def __init__(self):
-		LiteEthDepacketizer.__init__(self,
-			eth_phy_description(8),
-			eth_mac_description(8),
-			mac_header,
-			mac_header_len)
-
-class LiteEthMACPacketizer(LiteEthPacketizer):
-	def __init__(self):
-		LiteEthPacketizer.__init__(self,
-			eth_mac_description(8),
-			eth_phy_description(8),
-			mac_header,
-			mac_header_len)
-
-class LiteEthMACCrossbar(LiteEthCrossbar):
-	def __init__(self):
-		LiteEthCrossbar.__init__(self, LiteEthMACMasterPort, "ethernet_type")
-
-	def get_port(self, ethernet_type):
-		port = LiteEthMACUserPort(8)
-		if ethernet_type in self.users.keys():
-			raise ValueError("Ethernet type {0:#x} already assigned".format(ethernet_type))
-		self.users[ethernet_type] = port
-		return port
 
 class LiteEthMAC(Module, AutoCSR):
 	def __init__(self, phy, dw, interface="crossbar", endianness="big",
