0267868cbe870106d4ac61412cd503061e14bc64
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-02-25 10:40:44 -0700

    remove litex submodule

--- extcores/__init__.py
+++
@@ -1,4 +0,0 @@
-import os, sys
-sys.path.append(os.path.join("extcores", "litex", "litescope"))
-sys.path.append(os.path.join("extcores", "litex", "liteeth"))
-sys.path.append(os.path.join("extcores", "litex", "litesata"))
