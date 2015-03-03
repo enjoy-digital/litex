1d4dc4543633a20c61b13417d8e26c02fa50ca14
Florent Kermarrec <florent@enjoy-digital.fr>
2015-03-03 10:29:28 +0100

    LiteXXX cores: use format in prints

--- misoclib/tools/litescope/example_designs/test/test_regs.py
+++ misoclib/tools/litescope/example_designs/test/test_regs.py
@@ -2,8 +2,8 @@ def main(wb):
 	wb.open()
 	regs = wb.regs
 	###
-	print("sysid     : 0x%04x" %regs.identifier_sysid.read())
-	print("revision  : 0x%04x" %regs.identifier_revision.read())
-	print("frequency : %d MHz" %(regs.identifier_frequency.read()/1000000))
+	print("sysid     : 0x{:04x}".format(regs.identifier_sysid.read()))
+	print("revision  : 0x{:04x}".format(regs.identifier_revision.read()))
+	print("frequency : 0x{:04x}MHz".format(regs.identifier_frequency.read()/1000000))
 	###
 	wb.close()
