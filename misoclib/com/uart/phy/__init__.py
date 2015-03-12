767d45727a2c876c45689886f1562398a33a4628
Florent Kermarrec <florent@enjoy-digital.fr>
2015-03-12 16:57:38 +0100

    uart/liteeth: only import the phy we are going to use (UARTPHYSim cannot be imported on Windows since based on pty).

--- misoclib/com/uart/phy/__init__.py
+++ misoclib/com/uart/phy/__init__.py
@@ -1,12 +1,11 @@
 from misoclib.com.liteeth.common import *
 from misoclib.com.liteeth.generic import *
 
-from misoclib.com.uart.phy.sim import UARTPHYSim
-from misoclib.com.uart.phy.serial import UARTPHYSerial
-
 def UARTPHY(pads, *args, **kwargs):
 	# Autodetect PHY
 	if hasattr(pads, "source_stb"):
+		from misoclib.com.uart.phy.sim import UARTPHYSim
 		return UARTPHYSim(pads, *args, **kwargs)
 	else:
+		from misoclib.com.uart.phy.serial import UARTPHYSerial
 		return UARTPHYSerial(pads, *args, **kwargs)
