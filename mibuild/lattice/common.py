b5a9909b089933c230f6d0ce24c18f153a345cdf
Florent Kermarrec <florent@enjoy-digital.fr>
2015-03-16 22:57:18 +0100

    mibuild/xilinx/common: add LatticeDDROutput

--- mibuild/lattice/common.py
+++ mibuild/lattice/common.py
@@ -1,1 +1,18 @@
-lattice_special_overrides = {}
+from migen.fhdl.std import *
+from migen.genlib.io import *
+
+class LatticeDifferentialOutput:
+	@staticmethod
+	def lower(dr):
+		return LatticeDifferentialOutputImpl(dr.i, dr.o_p, dr.o_n)
+
+class LatticeDDROutputImpl(Module):
+	def __init__(self, i1, i2, o, clk):
+		self.specials += Instance("ODDRA",
+				i_CLK=clk, i_RST=0,
+				i_DA=i1, i_DB=i2, o_Q=o,
+		)
+
+lattice_special_overrides = {
+	DDROutput:	LatticeDDROutput
+}
