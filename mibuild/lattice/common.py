6dd8d89c6cf4aeb20a9d80559dbf8387cc18e1d0
Florent Kermarrec <florent@enjoy-digital.fr>
2015-03-17 09:40:25 +0100

    mibuild/lattice: fix LatticeDDROutput

--- mibuild/lattice/common.py
+++ mibuild/lattice/common.py
@@ -1,17 +1,17 @@
 from migen.fhdl.std import *
 from migen.genlib.io import *
 
-class LatticeDifferentialOutput:
-	@staticmethod
-	def lower(dr):
-		return LatticeDifferentialOutputImpl(dr.i, dr.o_p, dr.o_n)
-
 class LatticeDDROutputImpl(Module):
 	def __init__(self, i1, i2, o, clk):
 		self.specials += Instance("ODDRA",
 				i_CLK=clk, i_RST=0,
 				i_DA=i1, i_DB=i2, o_Q=o,
 		)
+
+class LatticeDDROutput:
+	@staticmethod
+	def lower(dr):
+		return LatticeDDROutputImpl(dr.i1, dr.i2, dr.o, dr.clk)
 
 lattice_special_overrides = {
 	DDROutput:	LatticeDDROutput
