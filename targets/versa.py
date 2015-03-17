cb4be529226d3d30aba71a854ea005066f3ce871
Florent Kermarrec <florent@enjoy-digital.fr>
2015-03-17 19:08:31 +0100

    targets: add Lattice ECP3 versa

---
+++ targets/versa.py
@@ -0,0 +1,17 @@
+from migen.fhdl.std import *
+from migen.bus import wishbone
+from migen.genlib.io import CRG
+
+from misoclib.soc import SoC
+
+class BaseSoC(SoC):
+	default_platform = "versa"
+	def __init__(self, platform, **kwargs):
+		SoC.__init__(self, platform,
+			clk_freq=100*1000000,
+			with_rom=True,
+			**kwargs)
+		self.submodules.crg = CRG(platform.request("clk100"), ~platform.request("rst_n"))
+		self.comb += platform.request("user_led", 0).eq(ResetSignal())
+
+default_subtarget = BaseSoC
