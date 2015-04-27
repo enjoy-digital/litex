1d9771f574e67c66b765e836420bb12586e7709b
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-04-27 13:42:32 +0800

    spiflash: use SoC defines, add write_to_flash function

--- targets/kc705.py
+++ targets/kc705.py
@@ -98,6 +98,8 @@ class BaseSoC(SDRAMSoC):
                                       i_CLK=0, i_GSR=0, i_GTS=0, i_KEYCLEARB=0, i_PACK=0,
                                       i_USRCCLKO=spiflash_pads.clk, i_USRCCLKTS=0, i_USRDONEO=1, i_USRDONETS=1)
             self.submodules.spiflash = spiflash.SpiFlash(spiflash_pads, dummy=11, div=2)
+            self.add_constant("SPIFLASH_PAGE_SIZE", 256)
+            self.add_constant("SPIFLASH_SECTOR_SIZE", 0x10000)
             self.flash_boot_address = 0xb00000
             self.register_rom(self.spiflash.bus)
 
