a8b8af220a8c03260899fe02a69e442a2e4fe2b0
Florent Kermarrec <florent@enjoy-digital.fr>
2015-05-01 20:20:20 +0200

    liteusb: add basic wishbone frontend (We could also reuse Etherbone in the future)

---
+++ misoclib/com/liteusb/frontend/wishbone.py
@@ -0,0 +1,14 @@
+from migen.fhdl.std import *
+
+from misoclib.com.liteusb.common import *
+from misoclib.tools.litescope.bridge.wishbone import LiteScopeWishboneBridge
+
+class LiteUSBWishboneBridge(LiteScopeWishboneBridge):
+    def __init__(self, port, clk_freq):
+        LiteScopeWishboneBridge.__init__(self, port, clk_freq)
+        self.comb += [
+            port.sink.sop.eq(1),
+            port.sink.eop.eq(1),
+            port.sink.length.eq(1),
+            port.sink.dst.eq(port.tag)
+        ]
