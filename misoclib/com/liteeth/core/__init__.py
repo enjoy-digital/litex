0545d492949ee519d464bfbee8caf7421220e2cb
Florent Kermarrec <florent@enjoy-digital.fr>
2015-07-06 21:23:19 +0200

    liteeth/core: add with_icmp parameter

--- misoclib/com/liteeth/core/__init__.py
+++ misoclib/com/liteeth/core/__init__.py
@@ -7,14 +7,15 @@ from misoclib.com.liteeth.core.icmp import LiteEthICMP
 
 
 class LiteEthIPCore(Module, AutoCSR):
-    def __init__(self, phy, mac_address, ip_address, clk_freq):
+    def __init__(self, phy, mac_address, ip_address, clk_freq, with_icmp=True):
         self.submodules.mac = LiteEthMAC(phy, 8, interface="crossbar", with_preamble_crc=True)
         self.submodules.arp = LiteEthARP(self.mac, mac_address, ip_address, clk_freq)
         self.submodules.ip = LiteEthIP(self.mac, mac_address, ip_address, self.arp.table)
-        self.submodules.icmp = LiteEthICMP(self.ip, ip_address)
+        if with_icmp:
+        	self.submodules.icmp = LiteEthICMP(self.ip, ip_address)
 
 
 class LiteEthUDPIPCore(LiteEthIPCore):
-    def __init__(self, phy, mac_address, ip_address, clk_freq):
-        LiteEthIPCore.__init__(self, phy, mac_address, ip_address, clk_freq)
+    def __init__(self, phy, mac_address, ip_address, clk_freq, with_icmp=True):
+        LiteEthIPCore.__init__(self, phy, mac_address, ip_address, clk_freq, with_icmp)
         self.submodules.udp = LiteEthUDP(self.ip, ip_address)
