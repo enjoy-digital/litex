8e8cc8e5a63a19bda429e53e62ef19e4e3e99584
Florent Kermarrec <florent@enjoy-digital.fr>
2015-09-07 12:44:47 +0200

    move liteusb to a separate repo (https://github.com/enjoy-digital/liteusb)

--- misoclib/com/liteusb/frontend/wishbone.py
+++
@@ -1,9 +0,0 @@
-from migen.fhdl.std import *
-
-from misoclib.com.liteusb.common import *
-from misoclib.tools.wishbone import WishboneStreamingBridge
-
-class LiteUSBWishboneBridge(WishboneStreamingBridge):
-    def __init__(self, port, clk_freq):
-        WishboneStreamingBridge.__init__(self, port, clk_freq)
-        self.comb += port.sink.dst.eq(port.tag)
