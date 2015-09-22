6005548df6dd173d35e89873564e00f1b4fe8f5d
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-09-22 17:27:27 +0800

    setup.py: cleanup

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, "README.md")).read()
 
 required_version = (3, 3)
 if sys.version_info < required_version:
@@ -17,12 +15,12 @@ setup(
     name="migen",
     version="1.0",
     description="Python toolbox for building complex digital hardware",
-    long_description=README,
+    long_description=open("README.md").read(),
     author="Sebastien Bourdeauducq",
     author_email="sb@m-labs.hk",
     url="http://m-labs.hk",
     download_url="https://github.com/m-labs/migen",
-    packages=find_packages(here),
+    packages=find_packages(),
     test_suite="migen.test",
     license="BSD",
     platforms=["Any"],
