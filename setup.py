01be953e30e936f0ddea1729185c2de5f1af41f2
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-09-23 09:52:12 +0800

    setup: cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 from setuptools import setup
 from setuptools import find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, "README")).read()
 
 required_version = (3, 3)
 if sys.version_info < required_version:
@@ -15,14 +12,14 @@ if sys.version_info < required_version:
 
 setup(
     name="misoc",
-    version="unknown",
+    version="1.0",
     description="a high performance and small footprint SoC based on Migen",
-    long_description=README,
+    long_description=open("README.rst").read(),
     author="Sebastien Bourdeauducq",
     author_email="sb@m-labs.hk",
     url="http://m-labs.hk",
     download_url="https://github.com/m-labs/misoc",
-    packages=find_packages(here),
+    packages=find_packages(),
     license="BSD",
     platforms=["Any"],
     keywords="HDL ASIC FPGA hardware design",
