8817716d5ffa323205cb338d641fc0878a53fd65
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-10-13 18:39:41 +0800

    test/divider: subtests

--- migen/test/test_divider.py
+++ migen/test/test_divider.py
@@ -14,13 +14,14 @@ class DivisionCase(SimCase, unittest.TestCase):
         def gen():
             for dividend in range(16):
                 for divisor in range(1, 16):
-                    yield self.tb.dut.dividend_i.eq(dividend)
-                    yield self.tb.dut.divisor_i.eq(divisor)
-                    yield self.tb.dut.start_i.eq(1)
-                    yield
-                    yield self.tb.dut.start_i.eq(0)
-                    while not (yield self.tb.dut.ready_o):
+                    with self.subTest(dividend=dividend, divisor=divisor):
+                        yield self.tb.dut.dividend_i.eq(dividend)
+                        yield self.tb.dut.divisor_i.eq(divisor)
+                        yield self.tb.dut.start_i.eq(1)
                         yield
-                    self.assertEqual((yield self.tb.dut.quotient_o), dividend//divisor)
-                    self.assertEqual((yield self.tb.dut.remainder_o), dividend%divisor)
+                        yield self.tb.dut.start_i.eq(0)
+                        while not (yield self.tb.dut.ready_o):
+                            yield
+                        self.assertEqual((yield self.tb.dut.quotient_o), dividend//divisor)
+                        self.assertEqual((yield self.tb.dut.remainder_o), dividend%divisor)
         self.run_with(gen())
