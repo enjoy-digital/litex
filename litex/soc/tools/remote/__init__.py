68c2d3b7a0022247d4091d8854ce2733d69918dd
Florent Kermarrec <florent@enjoy-digital.fr>
2016-02-18 12:55:18 +0100

    soc/tools/remove: fix import

--- litex/soc/tools/remote/__init__.py
+++ litex/soc/tools/remote/__init__.py
@@ -1,3 +1,3 @@
 from litex.soc.tools.remote.comm_uart import CommUART
-from litex.soc.tools.remote.server import RemoteServer
-from litex.soc.tools.remote.client import RemoteClient
+from litex.soc.tools.remote.litex_server import RemoteServer
+from litex.soc.tools.remote.litex_client import RemoteClient
