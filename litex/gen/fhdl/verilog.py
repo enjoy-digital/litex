42767286ca011521ae11ae85bb421d0e56c2f712
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-26 17:13:41 +0200

    gen/fhdl/verilog: add do in reserved_keywords

--- litex/gen/fhdl/verilog.py
+++ litex/gen/fhdl/verilog.py
@@ -27,7 +27,7 @@ _reserved_keywords = {
     "specify", "specparam", "strong0", "strong1", "supply0", "supply1",
     "table", "task", "time", "tran", "tranif0", "tranif1", "tri", "tri0",
     "tri1", "triand", "trior", "trireg", "unsigned", "use", "vectored", "wait",
-    "wand", "weak0", "weak1", "while", "wire", "wor","xnor", "xor"
+    "wand", "weak0", "weak1", "while", "wire", "wor","xnor", "xor", "do"
 }
 
 
