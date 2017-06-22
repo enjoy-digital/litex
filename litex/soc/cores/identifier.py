1364ac3657619bccd1b2adc398d592c493da5d5e
Florent Kermarrec <florent@enjoy-digital.fr>
2017-06-22 17:53:19 +0200

    soc/cores/identifier: append 0 to contents to indicate end of string

--- litex/soc/cores/identifier.py
+++ litex/soc/cores/identifier.py
@@ -5,8 +5,9 @@ class Identifier(Module):
     def __init__(self, ident):
         contents = list(ident.encode())
         l = len(contents)
-        if l > 256:
+        if l > 255:
             raise ValueError("Identifier string must be 255 characters or less")
+        contents.append(0)
         self.mem = Memory(8, len(contents), init=contents)
 
     def get_memories(self):
