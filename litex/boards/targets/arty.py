9eb1beea041498af7bf99ed2142d72283ce1fd48
Felix Held <felix-github@felixheld.de>
2018-01-12 13:54:10 +1100

    fix DDR3 on arty

--- litex/boards/targets/arty.py
+++ litex/boards/targets/arty.py
@@ -108,6 +108,8 @@ class BaseSoC(SoCSDRAM):
 
         # sdram
         self.submodules.ddrphy = a7ddrphy.A7DDRPHY(platform.request("ddram"))
+        self.add_constant("READ_LEVELING_BITSLIP", 3)
+        self.add_constant("READ_LEVELING_DELAY", 14)
         sdram_module = MT41K128M16(self.clk_freq, "1:4")
         self.register_sdram(self.ddrphy,
                             sdram_module.geom_settings,
