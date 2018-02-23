212e1a7076116a8322d1b2edf51200cfc5c116e7
Florent Kermarrec <florent@enjoy-digital.fr>
2018-02-23 13:36:32 +0100

    bump to 0.2.dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info[:3] < (3, 5):
 
 setup(
     name="litex",
-    version="0.1",
+    version="0.2.dev",
     description="Python tools to design FPGA cores and SoCs",
     long_description=open("README").read(),
     author="Florent Kermarrec",
