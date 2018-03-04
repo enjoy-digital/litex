da3189c8b5c7bdbf4e47dbf2e511ba121f450e10
Tim 'mithro' Ansell <me@mith.ro>
2018-03-03 16:46:38 -0800

    travis: Making the output more readable.

--- .travis-build-socs.sh
+++ .travis-build-socs.sh
@@ -1,15 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -x
 set -e
 
 source activate litex
 
 SOC_FILES=$(find litex/boards/targets -name \*.py | grep -v sim | grep -v "__")
 
+COUNT=4
+
 for SOC_FILE in $SOC_FILES; do
 	SOC=$(echo $SOC_FILE | sed -e's/\.py$//' -e's-/-.-g')
-	python -m $SOC --no-compile-gateware
-done
+	TARGET=$(echo $SOC | sed -e's/.*\.//')
 
-find soc_* | sort
+	travis_fold start "$SOC.1"
+	travis_time_start
+	echo "Building $TARGET ($SOC)"
+	echo "-------------------------------------------------------------------"
+	python -m $SOC --no-compile-gateware
+	echo "-------------------------------------------------------------------"
+	travis_time_finish
+	travis_fold end "$SOC.1"
+	travis_fold start "$SOC.2"
+	echo "Output of building $SOC"
+	echo "-------------------------------------------------------------------"
+	find soc_*$TARGET* | sort
+	echo "-------------------------------------------------------------------"
+	travis_fold end "$SOC.2"
+done
