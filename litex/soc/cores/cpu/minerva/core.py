fb24ac0ecc158bd1f7b69649bfafc356a709e5a3
Florent Kermarrec <florent@enjoy-digital.fr>
2018-09-06 16:40:30 +0200

    cpu/minerva: add workaround on import until code is released

--- litex/soc/cores/cpu/minerva/core.py
+++ litex/soc/cores/cpu/minerva/core.py
@@ -4,8 +4,6 @@ from migen import *
 
 from litex.soc.interconnect import wishbone
 
-from minerva.core import Minerva as MinervaCPU
-
 
 class Minerva(Module):
     def __init__(self, platform, cpu_reset_address, variant=None):
@@ -17,10 +15,15 @@ class Minerva(Module):
 
         ###
 
-        self.submodules.cpu = MinervaCPU(reset_address=cpu_reset_address)
-        self.comb += [
-            self.cpu.reset.eq(self.reset),
-            self.cpu.external_interrupt.eq(self.interrupt),
-            self.cpu.ibus.connect(self.ibus),
-            self.cpu.dbus.connect(self.dbus)
-        ]
+        try: # FIXME: workaround until Minerva code is released
+            from minerva.core import Minerva as MinervaCPU
+            self.submodules.cpu = MinervaCPU(reset_address=cpu_reset_address)
+            self.comb += [
+                self.cpu.reset.eq(self.reset),
+                self.cpu.external_interrupt.eq(self.interrupt),
+                self.cpu.ibus.connect(self.ibus),
+                self.cpu.dbus.connect(self.dbus)
+            ]
+        except:
+            pass
+
