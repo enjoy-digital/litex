200d413def22eed4d87bebd011944aa2bfd4c734
bunnie <bunnie@kosagi.com>
2019-06-02 22:26:18 +0000

    update stdint.h to include c99 types
    needed for some third party libraries to compile

--- litex/soc/software/include/base/stdint.h
+++ litex/soc/software/include/base/stdint.h
@@ -18,6 +18,15 @@ typedef int int32_t;
 typedef short int16_t;
 typedef char int8_t;
 
+typedef char int_least8_t;
+typedef unsigned char uint_least8_t;
+typedef short int_least16_t;
+typedef unsigned short uint_least16_t;
+typedef int int_least32_t;
+typedef unsigned int uint_least32_t;
+typedef long long int_least64_t;
+typedef unsigned long long uint_least64_t;
+  
 #define __int_c_join(a, b) a ## b
 #define __int_c(v, suffix) __int_c_join(v, suffix)
 #define __uint_c(v, suffix) __int_c_join(v##U, suffix)
