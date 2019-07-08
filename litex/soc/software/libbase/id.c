e42f33ede1025438a175321d4e8cb9a372397070
Gabriel L. Somlo <gsomlo@gmail.com>
2019-07-08 09:43:40 -0400

    soc_core: additional csr_alignment follow-up fixes
    - Update a few additional places to use DFII_ADDR_SHIFT instead of
      a hard-coded 4, which assumed 32-bit alignment.
    
    - Force 64-bit alignment Rocket -- the only supported configuration!
    
    This is a fixup for commit f4770219, tested on Rocket and 64bit Linux.
    
    Signed-off-by: Gabriel Somlo <gsomlo@gmail.com>

--- litex/soc/software/libbase/id.c
+++ litex/soc/software/libbase/id.c
@@ -4,13 +4,14 @@
 #include <string.h>
 #include <id.h>
 
+#define DFII_ADDR_SHIFT CONFIG_CSR_ALIGNMENT/8
 
 void get_ident(char *ident)
 {
 #ifdef CSR_IDENTIFIER_MEM_BASE
     int i;
     for(i=0;i<256;i++)
-        ident[i] = MMPTR(CSR_IDENTIFIER_MEM_BASE + 4*i);
+        ident[i] = MMPTR(CSR_IDENTIFIER_MEM_BASE + DFII_ADDR_SHIFT*i);
 #else
     ident[0] = 0;
 #endif
