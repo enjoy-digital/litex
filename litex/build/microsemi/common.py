60873a5b73ec172681bb00fc3172bf113c0d1c20
Florent Kermarrec <florent@enjoy-digital.fr>
2019-08-08 16:06:40 +0200

    microsemi/common: improve presentation

--- litex/build/microsemi/common.py
+++ litex/build/microsemi/common.py
@@ -9,10 +9,14 @@ class MicrosemiPolarfireAsyncResetSynchronizerImpl(Module):
     def __init__(self, cd, async_reset):
         rst1 = Signal()
         self.specials += [
-            Instance("DFN1P0", i_D=0, i_PRE=~async_reset,
-                     i_CLK=cd.clk, o_Q=rst1),
-            Instance("DFN1P0", i_D=rst1, i_PRE=~async_reset,
-                     i_CLK=cd.clk, o_Q=cd.rst)
+            Instance("DFN1P0",
+                i_D=0, i_PRE=~async_reset,
+                i_CLK=cd.clk, o_Q=rst1
+            ),
+            Instance("DFN1P0",
+                i_D=rst1, i_PRE=~async_reset,
+                i_CLK=cd.clk, o_Q=cd.rst
+            )
         ]
 
 
