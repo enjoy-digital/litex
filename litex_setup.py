3e30c64842de986f59f5703ce060852b94c2d364
Florent Kermarrec <florent@enjoy-digital.fr>
2019-08-26 09:27:19 +0200

    litex_setup: add litex-boards

--- litex_setup.py
+++ litex_setup.py
@@ -9,8 +9,13 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 
 # name,  (url, recursive clone, develop)
 repos = [
+    # HDL
     ("migen",      ("https://github.com/m-labs/",        True,  True)),
+
+    # LiteX SoC builder
     ("litex",      ("https://github.com/enjoy-digital/", True,  True)),
+
+    # LiteX cores ecosystem
     ("liteeth",    ("https://github.com/enjoy-digital/", False, True)),
     ("litedram",   ("https://github.com/enjoy-digital/", False, True)),
     ("litepcie",   ("https://github.com/enjoy-digital/", False, True)),
@@ -19,6 +24,9 @@ repos = [
     ("liteiclink", ("https://github.com/enjoy-digital/", False, True)),
     ("litevideo",  ("https://github.com/enjoy-digital/", False, True)),
     ("litescope",  ("https://github.com/enjoy-digital/", False, True)),
+
+    # LiteX boards support
+    ("litex-boards",   ("https://github.com/litex-hub/", False, True)),
 ]
 repos = OrderedDict(repos)
 
