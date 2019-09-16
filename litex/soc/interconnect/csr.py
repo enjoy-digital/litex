9bda614a3ebcbe8cb066a9748a2e4bcd49436500
Florent Kermarrec <florent@enjoy-digital.fr>
2019-09-16 08:48:05 +0200

    csr: update copyrights

--- litex/soc/interconnect/csr.py
+++ litex/soc/interconnect/csr.py
@@ -1,6 +1,7 @@
 # This file is Copyright (c) 2015 Sebastien Bourdeauducq <sb@m-labs.hk>
-# This file is Copyright (c) 2015-2018 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2015-2019 Florent Kermarrec <florent@enjoy-digital.fr>
 # This file is Copyright (c) 2016-2019 Tim 'mithro' Ansell <me@mith.ro>
+# This file is Copyright (c) 2019 Sean Cross <sean@xobs.io>
 # License: BSD
 
 
