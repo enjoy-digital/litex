4d90058b180ff43856da47f03b7bc736f16b887d
Florent Kermarrec <florent@enjoy-digital.fr>
2019-09-30 11:32:07 +0200

    soc/integration: move cpu_interface retro-compatibility to litex/__init__

--- litex/__init__.py
+++ litex/__init__.py
@@ -1,1 +1,6 @@
+import sys
 from litex.tools.litex_client import RemoteClient
+
+# retro-compat 2019-09-29
+from litex.soc.integration import export
+sys.modules["litex.soc.integration.cpu_interface"] = export
