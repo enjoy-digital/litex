a54b80b9b4eaa6defca99b0749da8426535bbb62
Florent Kermarrec <florent@enjoy-digital.fr>
2019-10-30 16:33:40 +0100

    targets: use type="io" instead of io_region=True

--- litex/boards/targets/simple.py
+++ litex/boards/targets/simple.py
@@ -44,7 +44,7 @@ class EthernetSoC(BaseSoC):
         self.submodules.ethmac = LiteEthMAC(phy=self.ethphy, dw=32,
             interface="wishbone", endianness=self.cpu.endianness, with_preamble_crc=False)
         self.add_wb_slave(self.mem_map["ethmac"], self.ethmac.bus, 0x2000)
-        self.add_memory_region("ethmac", self.mem_map["ethmac"], 0x2000, io_region=True)
+        self.add_memory_region("ethmac", self.mem_map["ethmac"], 0x2000, type="io")
         self.add_csr("ethmac")
         self.add_interrupt("ethmac")
 
