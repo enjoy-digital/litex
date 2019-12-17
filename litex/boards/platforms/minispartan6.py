76e57414c3b7a5e5c50803d6b9078c0ab46ff1aa
Florent Kermarrec <florent@enjoy-digital.fr>
2019-12-17 09:47:12 +0100

    platforms/minispartan6: add assert on available devices

--- litex/boards/platforms/minispartan6.py
+++ litex/boards/platforms/minispartan6.py
@@ -118,6 +118,7 @@ class Platform(XilinxPlatform):
     default_clk_period = 1e9/32e6
 
     def __init__(self, device="xc6slx25"):
+        assert device in ["xc6slx9", "xc6slx25"]
         XilinxPlatform.__init__(self, device+"-3-ftg256", _io, _connectors)
 
     def create_programmer(self):
