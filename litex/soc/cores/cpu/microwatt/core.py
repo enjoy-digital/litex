16e7c6b6340184c208b40ccc5005a9cd9911d76e
Florent Kermarrec <florent@enjoy-digital.fr>
2019-12-17 09:27:19 +0100

    cpu/microwatt: update copyright

--- litex/soc/cores/cpu/microwatt/core.py
+++ litex/soc/cores/cpu/microwatt/core.py
@@ -1,4 +1,5 @@
 # This file is Copyright (c) 2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2019 Benjamin Herrenschmidt <benh@ozlabs.org>
 # License: BSD
 
 import os
