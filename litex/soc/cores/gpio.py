502a766c0ccd1a22043db405a5136955a680ef50
Tom Keddie <git@bronwenandtom.com>
2020-02-20 07:17:52 -0800

    soc/core/gpio: Add GPIOBidirectional

--- litex/soc/cores/gpio.py
+++ litex/soc/cores/gpio.py
@@ -43,3 +43,21 @@ class GPIOTristate(Module, AutoCSR):
         self.comb += t.oe.eq(self._oe.storage)
         self.comb += t.o.eq(self._out.storage)
         self.specials += MultiReg(t.i, self._in.status)
+
+# GPIO Bidirectional -------------------------------------------------------------------------------
+
+class GPIOBidirectional(Module, AutoCSR):
+    def __init__(self, pads):
+        self._pins_in = CSRStatus(len(pads))
+        self._pins_out = CSRStorage(len(pads))
+        self._pins_oe = CSRStorage(len(pads))
+        gpio_pins_t = [None] * len(pads)
+        bit = 0
+        for pin_group in pads.layout:
+            for pin in getattr(pads, pin_group[0]):
+                gpio_pins_t[bit] = TSTriple()
+                self.specials += gpio_pins_t[bit].get_tristate(pin)
+                self.comb += gpio_pins_t[bit].o.eq(self._pins_out.storage[bit])
+                self.comb += gpio_pins_t[bit].oe.eq(self._pins_oe.storage[bit])
+                self.comb += self._pins_in.status[bit].eq(gpio_pins_t[bit].i)
+                bit=bit+1
