30d25ffe5b2481368de3dad2f6d4dbf8b08b0f63
Florent Kermarrec <florent@enjoy-digital.fr>
2020-04-07 11:48:16 +0200

    setup.py: simplify, switch to Python3.6+ (using python_requires), remove version.
    - Deprecate Python 3.5, switch to Python 3.6+.
    - Remove which was not used or updated. We'll see to get this back when working on releases.

--- setup.py
+++ setup.py
@@ -1,38 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
 from setuptools import setup
 from setuptools import find_packages
 
 
-if sys.version_info[:3] < (3, 5):
-    raise SystemExit("You need Python 3.5+")
-
-
 setup(
     name="litex",
-    version="0.2.dev",
-    description="Python tools to design FPGA cores and SoCs",
-    long_description=open("README.md").read(),
+    description="Python SoC/Core builder for building FPGA based systems.",
     author="Florent Kermarrec",
     author_email="florent@enjoy-digital.fr",
     url="http://enjoy-digital.fr",
     download_url="https://github.com/enjoy-digital/litex",
     test_suite="test",
     license="BSD",
-    platforms=["Any"],
-    keywords="HDL ASIC FPGA hardware design",
-    classifiers=[
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-        "Environment :: Console",
-        "Development Status :: Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-    ],
-    packages=find_packages(exclude=("test*", "sim*", "doc*")),
+    python_requires="~=3.6",
     install_requires=["migen", "pyserial"],
+    packages=find_packages(exclude=("test*", "sim*", "doc*")),
     include_package_data=True,
     entry_points={
         "console_scripts": [
