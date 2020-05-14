3391398a5fccd5fc6cae36231a11d34f032dc3c5
Florent Kermarrec <florent@enjoy-digital.fr>
2020-05-14 15:20:52 +0200

    bios/sdram: always show bitslip on two digits to keep scan aligned.

--- litex/soc/software/bios/sdram.c
+++ litex/soc/software/bios/sdram.c
@@ -552,7 +552,7 @@ static int read_level_scan(int module, int bitslip)
 	sdram_dfii_pird_baddress_write(0);
 	score = 0;
 
-	printf("m%d, b%d: |", module, bitslip);
+	printf("m%d, b%02d: |", module, bitslip);
 	read_delay_rst(module);
 	for(i=0;i<SDRAM_PHY_DELAYS;i++) {
 		int working = 1;
@@ -978,7 +978,7 @@ static void read_leveling(void)
 		}
 
 		/* select best read window */
-		printf("best: m%d, b%d ", module, best_bitslip);
+		printf("best: m%d, b%02d ", module, best_bitslip);
 		read_bitslip_rst(module);
 		for (bitslip=0; bitslip<best_bitslip; bitslip++)
 			read_bitslip_inc(module);
