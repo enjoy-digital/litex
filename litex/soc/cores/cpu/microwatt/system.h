68d2aa45fa90d3f3809e762ae575411d626bef1d
Joel Stanley <joel@jms.id.au>
2020-06-10 10:39:16 +0930

    microwatt: Add icache flush

--- litex/soc/cores/cpu/microwatt/system.h
+++ litex/soc/cores/cpu/microwatt/system.h
@@ -5,8 +5,11 @@
 extern "C" {
 #endif
 
-__attribute__((unused)) static void flush_cpu_icache(void){}; /* FIXME: do something useful here! */
-__attribute__((unused)) static void flush_cpu_dcache(void){}; /* FIXME: do something useful here! */
+static inline void flush_cpu_icache(void)
+{
+	__asm__ volatile ("icbi 0,0; isync" : : : "memory");
+}
+static inline void flush_cpu_dcache(void){}; /* FIXME: do something useful here! */
 void flush_l2_cache(void);
 
 void busy_wait(unsigned int ms);
