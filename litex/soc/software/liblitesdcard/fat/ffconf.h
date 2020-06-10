08bef5fc4cc98e0e0eb5d9068456bcc1fe24f949
Florent Kermarrec <florent@enjoy-digital.fr>
2020-06-10 11:46:59 +0200

    software/liblitesdcard/ffconf: enable FF_FS_MINIMIZE and FF_FS_TINY.

--- litex/soc/software/liblitesdcard/fat/ffconf.h
+++ litex/soc/software/liblitesdcard/fat/ffconf.h
@@ -15,7 +15,7 @@
 /  and optional writing functions as well. */
 
 
-#define FF_FS_MINIMIZE	0
+#define FF_FS_MINIMIZE	1
 /* This option defines minimization level to remove some basic API functions.
 /
 /   0: Basic functions are fully enabled.
@@ -221,7 +221,7 @@
 / System Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_FS_TINY		0
+#define FF_FS_TINY		1
 /* This option switches tiny buffer configuration. (0:Normal or 1:Tiny)
 /  At the tiny configuration, size of file object (FIL) is shrinked FF_MAX_SS bytes.
 /  Instead of private sector buffer eliminated from the file object, common sector
