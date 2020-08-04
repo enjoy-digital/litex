abc49964ea6719866684b474e10167950b85854e
Florent Kermarrec <florent@enjoy-digital.fr>
2020-08-04 16:38:02 +0200

    tools/litex_json2dts: add missing copyrights.

--- litex/tools/litex_json2dts.py
+++ litex/tools/litex_json2dts.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# This file is Copyright (c) 2019-2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2020 Antmicro <www.antmicro.com>
+# License: BSD
+
 import sys
 import json
 import argparse
