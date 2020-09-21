enjoy-digital/litex

commit a601415bf060a8dcd1ac383787bc670aeb89db20

software/liblitedram: add functions/commands to reset and force cmd/dat write leveling delays.
Useful to investigate/speed-up new board support.

litex/soc/software/bios/cmds/cmd_litedram.c
@@ -34,21 +34,124 @@ define_command(sdram_init, sdram_init, "Initialize SDRAM (Init + Calibration)", 
 define_command(sdram_cal, sdram_calibration, "Calibrate SDRAM", LITEDRAM_CMDS);
 #endif
 
+#ifdef CSR_DDRPHY_CDLY_RST_ADDR
+
 /**
- * Command "sdram_mrw"
+ * Command "sdram_rst_cmd_delay"
+ *
+ * Reset write leveling Cmd delay
+ *
+ */
+#if defined(CSR_SDRAM_BASE) && defined(CSR_DDRPHY_BASE)
+static void sdram_rst_cmd_delay_handler(int nb_params, char **params)
+{
+	sdram_write_leveling_rst_cmd_delay(1);
+}
+define_command(sdram_rst_cmd_delay, sdram_rst_cmd_delay_handler, "Reset write leveling Cmd delay", LITEDRAM_CMDS);
+#endif
+
+/**
+ * Command "sdram_force_cmd_delay"
+ *
+ * Force write leveling Cmd delay
+ *
+ */
+#if defined(CSR_SDRAM_BASE) && defined(CSR_DDRPHY_BASE)
+static void sdram_force_cmd_delay_handler(int nb_params, char **params)
+{
+	char *c;
+	int taps;
+	if (nb_params < 1) {
+		printf("sdram_force_cmd_delay <taps>");
+		return;
+	}
+	taps = strtoul(params[0], &c, 0);
+	if (*c != 0) {
+		printf("Incorrect taps");
+		return;
+	}
+	sdram_write_leveling_force_cmd_delay(taps, 1);
+}
+define_command(sdram_force_cmd_delay, sdram_force_cmd_delay_handler, "Force write leveling Cmd delay", LITEDRAM_CMDS);
+#endif
+
+#endif
+
+#ifdef CSR_DDRPHY_WDLY_DQ_RST_ADDR
+
+/**
+ * Command "sdram_rst_dat_delay"
+ *
+ * Reset write leveling Dat delay
+ *
+ */
+#if defined(CSR_SDRAM_BASE) && defined(CSR_DDRPHY_BASE)
+static void sdram_rst_dat_delay_handler(int nb_params, char **params)
+{
+	char *c;
+	int module;
+	if (nb_params < 1) {
+		printf("sdram_rst_dat_delay <module>");
+		return;
+	}
+	module = strtoul(params[0], &c, 0);
+	if (*c != 0) {
+		printf("Incorrect module");
+		return;
+	}
+	sdram_write_leveling_rst_dat_delay(module, 1);
+}
+define_command(sdram_rst_dat_delay, sdram_rst_dat_delay_handler, "Reset write leveling Dat delay", LITEDRAM_CMDS);
+#endif
+
+/**
+ * Command "sdram_force_dat_delay"
+ *
+ * Force write leveling Dat delay
+ *
+ */
+#if defined(CSR_SDRAM_BASE) && defined(CSR_DDRPHY_BASE)
+static void sdram_force_dat_delay_handler(int nb_params, char **params)
+{
+	char *c;
+	int module;
+	int taps;
+	if (nb_params < 2) {
+		printf("sdram_force_dat_delay <module> <taps>");
+		return;
+	}
+	module = strtoul(params[0], &c, 0);
+	if (*c != 0) {
+		printf("Incorrect module");
+		return;
+	}
+	taps = strtoul(params[1], &c, 0);
+	if (*c != 0) {
+		printf("Incorrect taps");
+		return;
+	}
+	sdram_write_leveling_force_dat_delay(module, taps, 1);
+}
+define_command(sdram_force_dat_delay, sdram_force_dat_delay_handler, "Reset write leveling Dat delay", LITEDRAM_CMDS);
+#endif
+
+#endif
+
+/**
+ * Command "sdram_mr_write"
  *
  * Write SDRAM Mode Register
  *
  */
 #if defined(CSR_SDRAM_BASE) && defined(CSR_DDRPHY_BASE)
-static void sdram_mrw_handler(int nb_params, char **params)
+static void sdram_mr_write_handler(int nb_params, char **params)
 {
 	char *c;
 	uint8_t reg;
 	uint16_t value;
 
 	if (nb_params < 2) {
-		printf("sdram_mrw <reg> <value>");
+		printf("sdram_mr_write <reg> <value>");
 		return;
 	}
 	reg = strtoul(params[0], &c, 0);
@@ -64,7 +167,7 @@ static void sdram_mrw_handler(int nb_params, char **params)
 	printf("Writing 0x%04x to MR%d", value, reg);
 	sdram_mode_register_write(reg, value);
 }
-define_command(sdram_mrw, sdram_mrw_handler, "Write SDRAM Mode Register", LITEDRAM_CMDS);
+define_command(sdram_mr_write, sdram_mr_write_handler, "Write SDRAM Mode Register", LITEDRAM_CMDS);
 #endif
 
 /**

litex/soc/software/bios/cmds/cmd_bios.c
@@ -29,7 +29,7 @@ static void help_handler(int nb_params, char **params)
 		not_empty = 0;
 		for (cmd = __bios_cmd_start; cmd != __bios_cmd_end; cmd++) {
 			if ((*cmd)->group == i) {
-				printf("%-16s - %s\n", (*cmd)->name, (*cmd)->help ? (*cmd)->help : "-");
+				printf("%-24s - %s\n", (*cmd)->name, (*cmd)->help ? (*cmd)->help : "-");
 				not_empty = 1;
 			}
 		}

litex/soc/software/liblitedram/sdram.c
@@ -95,7 +95,8 @@ void sdram_mode_register_write(char reg, int value) {
 
 #ifdef SDRAM_PHY_WRITE_LEVELING_CAPABLE
 
-int sdrwl_delays[16];
+int _sdram_write_leveling_cmd_scan = 1;
+int _sdram_write_leveling_cmd_delays[16];
 
 static void sdram_write_leveling_on(void)
 {
@@ -125,6 +126,42 @@ static void sdram_write_leveling_off(void)
 #endif
 
 	ddrphy_wlevel_en_write(0);
+}
+
+void sdram_write_leveling_rst_cmd_delay(int show) {
+	_sdram_write_leveling_cmd_scan = 1;
+	if (show)
+		printf("Reseting Cmd delay\n");
+}
+
+void sdram_write_leveling_force_cmd_delay(int taps, int show) {
+	_sdram_write_leveling_cmd_scan = 0;
+#if CSR_DDRPHY_EN_VTC_ADDR
+	ddrphy_en_vtc_write(0);
+#endif
+	ddrphy_cdly_rst_write(1);
+	while (taps > 0) {
+		ddrphy_cdly_inc_write(1);
+		cdelay(1000);
+		taps--;
+	}
+#if CSR_DDRPHY_EN_VTC_ADDR
+	ddrphy_en_vtc_write(1);
+#endif
+	if (show)
+		printf("Forcing Cmd delay to %d taps\n", taps);
+}
+
+void sdram_write_leveling_rst_dat_delay(int module, int show) {
+	_sdram_write_leveling_cmd_delays[module] = -1;
+	if (show)
+		printf("Reseting Dat delay of module %d\n", module);
+}
+
+void sdram_write_leveling_force_dat_delay(int module, int taps, int show) {
+	_sdram_write_leveling_cmd_delays[module] = taps;
+	if (show)
+		printf("Forcing Dat delay of module %d to %d taps\n", module, taps);
 }
 
 static void sdram_write_leveling_rst_delay(int module) {
@@ -244,8 +281,8 @@ static int sdram_write_leveling_scan(int *delays, int loops, int show)
 		sdram_write_leveling_rst_delay(i);
 
 		/* use forced delay if configured */
-		if (sdrwl_delays[i] >= 0) {
-			delays[i] = sdrwl_delays[i];
+		if (_sdram_write_leveling_cmd_delays[i] >= 0) {
+			delays[i] = _sdram_write_leveling_cmd_delays[i];
 
 			/* configure write delay */
 			for(j=0; j<delays[i]; j++)
@@ -698,8 +735,6 @@ void sdram_read_leveling(void)
 /* Leveling                                                              */
 /*-----------------------------------------------------------------------*/
 
-int _sdram_write_leveling_cdly_scan = 1;
-
 int sdram_leveling(void)
 {
 	int module;
@@ -715,7 +750,7 @@ int sdram_leveling(void)
 
 #ifdef SDRAM_PHY_WRITE_LEVELING_CAPABLE
 	printf("Write leveling:\n");
-	if (_sdram_write_leveling_cdly_scan) {
+	if (_sdram_write_leveling_cmd_scan) {
 		sdram_write_leveling();
 	} else {
 		/* use only the current cdly */
@@ -764,7 +799,8 @@ int sdram_init(void)
 {
 #ifdef SDRAM_PHY_WRITE_LEVELING_CAPABLE
 	int i;
-	for (i=0; i<16; i++) sdrwl_delays[i] = -1; /* disable forced delays */
+	sdram_write_leveling_rst_cmd_delay(0);
+	for (i=0; i<16; i++) sdram_write_leveling_rst_dat_delay(i, 0);
 #endif
 	printf("Initializing SDRAM @0x%08x...\n", MAIN_RAM_BASE);
 	sdram_software_control_on();

litex/soc/software/liblitedram/sdram.h
@@ -23,6 +23,10 @@ void sdram_mode_register_write(char reg, int value);
 /*-----------------------------------------------------------------------*/
 /* Write Leveling                                                        */
 /*-----------------------------------------------------------------------*/
+void sdram_write_leveling_rst_cmd_delay(int show);
+void sdram_write_leveling_force_cmd_delay(int taps, int show);
+void sdram_write_leveling_rst_dat_delay(int module, int show);
+void sdram_write_leveling_force_dat_delay(int module, int taps, int show);
 int sdram_write_leveling(void);
 
 /*-----------------------------------------------------------------------*/

commit 381100724b1e2dd2cfd481d4e347299ef8e608ed

software/liblitedram: fix conditional compilation bug
Fix conditional compilation bug introduced by a601415b.

litex/soc/software/bios/cmds/cmd_litedram.c
@@ -34,7 +34,7 @@ define_command(sdram_init, sdram_init, "Initialize SDRAM (Init + Calibration)", 
 define_command(sdram_cal, sdram_calibration, "Calibrate SDRAM", LITEDRAM_CMDS);
 #endif
 
-#ifdef CSR_DDRPHY_CDLY_RST_ADDR
+#if defined(CSR_DDRPHY_CDLY_RST_ADDR) && defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE)
 
 /**
  * Command "sdram_rst_cmd_delay"