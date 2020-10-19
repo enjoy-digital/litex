066a8a0659f23ee1ceed9aa44a9bc9930c6ff3fd
Stephane Gourichon <stephane.gourichon@fidergo.fr>
2020-10-19 15:21:06 +0200

    DFUProg now checks and propagate return codes of called executables so that failure gets noticed.

--- litex/build/dfu.py
+++ litex/build/dfu.py
@@ -19,6 +19,11 @@ class DFUProg(GenericProgrammer):
         self.pid = pid
 
     def load_bitstream(self, bitstream_file):
-        subprocess.call(["cp", bitstream_file, bitstream_file + ".dfu"])
-        subprocess.call(["dfu-suffix", "-v", self.vid, "-p", self.pid, "-a", bitstream_file + ".dfu"])
-        subprocess.call(["dfu-util", "--download", bitstream_file + ".dfu", "-R"])
+        rc = subprocess.call(["cp", bitstream_file, bitstream_file + ".dfu"])
+        if (rc != 0):
+            return rc
+        rc = subprocess.call(["dfu-suffix", "-v", self.vid, "-p", self.pid, "-a", bitstream_file + ".dfu"])
+        if (rc != 0):
+            return rc
+        rc = subprocess.call(["dfu-util", "--download", bitstream_file + ".dfu", "-R"])
+            return rc
