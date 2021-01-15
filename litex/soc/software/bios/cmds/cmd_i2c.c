enjoy-digital/litex

commit ee1ea9baab8ae485c0e14a5e559a93dc16ac3b01

bios/cmd/cmd_i2c: make results similar to Linux's i2cdetect.

litex/soc/software/bios/cmds/cmd_i2c.c
@@ -132,15 +132,15 @@ static void i2c_scan_handler(int nb_params, char **params)
 {
 	int slave_addr;
 
-	printf("\n      0 1 2 3 4 5 6 7 8 9 a b c d e f");
+	printf("       0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f");
 	for (slave_addr = 0; slave_addr < 0x80; slave_addr++) {
 		if (slave_addr % 0x10 == 0) {
-			printf("\n0x%02x  ", (slave_addr/0x10) * 0x10);
+			printf("\n0x%02x:", (slave_addr/0x10) * 0x10);
 		}
 		if (i2c_poll(slave_addr)) {
-			printf("+ ");
+			printf(" %02x");
 		} else {
-			printf(". ");
+			printf(" --");
 		}
 	}
 	printf("\n");

commit 28ed06f1c5f69972211ec432449bc6e628d0c444

software/bios/cmds/cmd_i2c: Fix i2c_scan output
"i2c_scan" prints random data instead of the intended slave address:

    0x70: 10001ebc -- -- -- -- -- -- -- -- -- -- -- -- -- -- --

instead of:

    0x70: 70 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --

Fix this by adding the missing printf() parameter.

Fixes: ee1ea9baab8ae485 ("bios/cmd/cmd_i2c: make results similar to Linux's i2cdetect.")
Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

litex/soc/software/bios/cmds/cmd_i2c.c
@@ -138,7 +138,7 @@ static void i2c_scan_handler(int nb_params, char **params)
 			printf("\n0x%02x:", (slave_addr/0x10) * 0x10);
 		}
 		if (i2c_poll(slave_addr)) {
-			printf(" %02x");
+			printf(" %02x", slave_addr);
 		} else {
 			printf(" --");
 		}