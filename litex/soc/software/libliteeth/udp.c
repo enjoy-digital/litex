87ebdea5a736fe85d5050ef4b4694c1dc2660e52
Florent Kermarrec <florent@enjoy-digital.fr>
2021-04-29 11:47:58 +0200

    software/libliteeth: Add optional ETH_PHY_NO_RESET support to allow disabling software reset.
    Un-wanted when using the Hybrid LiteETHMAC since interrupt the hardware UDP/IP stack.

--- litex/soc/software/libliteeth/udp.c
+++ litex/soc/software/libliteeth/udp.c
@@ -463,10 +463,12 @@ void eth_init(void)
 {
 	printf("Ethernet init...\n");
 #ifdef CSR_ETHPHY_CRG_RESET_ADDR
+#ifndef ETH_PHY_NO_RESET
 	ethphy_crg_reset_write(1);
 	busy_wait(200);
 	ethphy_crg_reset_write(0);
 	busy_wait(200);
+#endif
 #endif
 }
 
