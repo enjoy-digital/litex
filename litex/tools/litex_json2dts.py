c06bd2c77d023aab739f056d31fecef0a53de283
Romain Dolbeau <romain@dolbeau.org>
2021-05-08 07:39:13 -0400

    Make the [ID]TLB size configurable from Litex ; expand the DTS to include cache/TLB/topology in CPUs & generate the required information for VexRiscv

--- litex/tools/litex_json2dts.py
+++ litex/tools/litex_json2dts.py
@@ -61,16 +61,60 @@ def generate_dts(d, initrd_start=None, initrd_size=None, polling=False):
 
     # VexRiscv-SMP
     if cpu_name == "vexriscv smp-linux":
+        # cache description
+        cache_desc = ""
+        if "cpu_dcache_size" in d["constants"]:
+            cache_desc += """
+                d-cache-size = <{d_cache_size}>;
+                d-cache-sets = <{d_cache_ways}>;
+                d-cache-block-size = <{d_cache_block_size}>;
+""".format(d_cache_size=d["constants"]["cpu_dcache_size"], d_cache_ways=d["constants"]["cpu_dcache_ways"], d_cache_block_size=d["constants"]["cpu_dcache_block_size"])
+        if "cpu_icache_size" in d["constants"]:
+            cache_desc += """
+                i-cache-size = <{i_cache_size}>;
+                i-cache-sets = <{i_cache_ways}>;
+                i-cache-block-size = <{i_cache_block_size}>;
+""".format(i_cache_size=d["constants"]["cpu_icache_size"], i_cache_ways=d["constants"]["cpu_icache_ways"], i_cache_block_size=d["constants"]["cpu_icache_block_size"])
+
+        # tlb description
+        tlb_desc = ""
+        if "cpu_dtlb_size" in d["constants"]:
+            tlb_desc += """
+                d-tlb-size = <{d_tlb_size}>;
+                d-tlb-sets = <{d_tlb_ways}>;
+""".format(d_tlb_size=d["constants"]["cpu_dtlb_size"], d_tlb_ways=d["constants"]["cpu_dtlb_ways"])
+        if "cpu_itlb_size" in d["constants"]:
+            tlb_desc += """
+                i-tlb-size = <{i_tlb_size}>;
+                i-tlb-sets = <{i_tlb_ways}>;
+""".format(i_tlb_size=d["constants"]["cpu_itlb_size"], i_tlb_ways=d["constants"]["cpu_itlb_ways"])
+
+        cpus = range(int(d["constants"]["config_cpu_count"]))
+
+        # topology
+        cpu_map = ""
+        if int(d["constants"]["config_cpu_count"]) > 1:
+            cpu_map += """
+            cpu-map {
+                cluster0 {"""
+            for cpu in cpus:
+                cpu_map += """
+                    core{cpu} {{
+                        cpu = <&CPU{cpu}>;
+                    }};""".format(cpu=cpu)
+            cpu_map += """
+                };
+            };"""
+
         dts += """
         cpus {{
             #address-cells = <1>;
             #size-cells    = <0>;
             timebase-frequency = <{sys_clk_freq}>;
 """.format(sys_clk_freq=d["constants"]["config_clock_frequency"])
-        cpus = range(int(d["constants"]["config_cpu_count"]))
         for cpu in cpus:
             dts += """
-            cpu@{cpu} {{
+            CPU{cpu}: cpu@{cpu} {{
                 device_type = "cpu";
                 compatible = "riscv";
                 riscv,isa = "{cpu_isa}";
@@ -78,16 +122,19 @@ def generate_dts(d, initrd_start=None, initrd_size=None, polling=False):
                 reg = <{cpu}>;
                 clock-frequency = <{sys_clk_freq}>;
                 status = "okay";
+                {cache_desc}
+                {tlb_desc}
                 L{irq}: interrupt-controller {{
                     #interrupt-cells = <0x00000001>;
                     interrupt-controller;
                     compatible = "riscv,cpu-intc";
                 }};
             }};
-""".format(cpu=cpu, irq=cpu, sys_clk_freq=d["constants"]["config_clock_frequency"], cpu_isa=d["constants"]["cpu_isa"])
+""".format(cpu=cpu, irq=cpu, sys_clk_freq=d["constants"]["config_clock_frequency"], cpu_isa=d["constants"]["cpu_isa"], cache_desc=cache_desc, tlb_desc=tlb_desc)
         dts += """
-        };
-"""
+            {cpu_map}
+        }};
+""".format(cpu_map=cpu_map)
 
     # mor1kx
     elif cpu_name == "mor1kx":
