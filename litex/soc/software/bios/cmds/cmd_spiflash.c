e6037c94f70969c01ef8b00ebac3f7a238bfa02b
Mateusz Kosmala <mkosmala@internships.antmicro.com>
2021-06-28 16:22:04 +0200

    cmds/cmd_spiflash.c: Add bios commands: flash_save_env, flash_show_env

--- litex/soc/software/bios/cmds/cmd_spiflash.c
+++ litex/soc/software/bios/cmds/cmd_spiflash.c
@@ -2,11 +2,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <generated/csr.h>
 
 #include "../command.h"
 #include "../helpers.h"
+#include <spiflash.h>
+#include <jsmn_helpers.h>
+#include <bios/boot.h>
 
 /**
  * Command "flash_write"
@@ -73,3 +77,68 @@ static void flash_erase_handler(int nb_params, char **params)
 define_command(flash_erase, flash_erase_handler, "Erase whole flash", SPIFLASH_CMDS);
 #endif
 
+/**
+ * Command "flash_save_env"
+ *
+ * Save environment parameters to SPI Flash storage
+ *
+ */
+#if (defined CSR_SPIFLASH_BASE && defined SPIFLASH_PAGE_SIZE && defined FLASH_SAVE_ENV)
+static void flash_save_env_handler(int nb_params, char **params)
+{
+	char base_env_params[ENV_VAR_SIZE];
+	memset(base_env_params, 0, sizeof(base_env_params));
+	unsigned int addr;
+	char *c;
+	if (nb_params > 1)
+	{
+		printf("flash_save_env <address>");
+		return;
+	}
+	if(nb_params == 0)
+		addr = (unsigned int)FLASH_ENV_ADDRESS;
+	else
+	{
+		addr = strtoul(params[0], &c, 0);
+		if (*c != 0)
+		{
+			printf("Incorrect address");
+			return;
+		}
+	}
+	erase_flash_subsector(addr);
+	get_env_params(base_env_params, sizeof(base_env_params)); /*Getting environment parameters in JSON format*/
+	print_tokens(base_env_params, NULL);
+	write_to_flash(addr, (const unsigned char *)base_env_params, sizeof(base_env_params));
+}
+
+define_command(flash_save_env, flash_save_env_handler, "Save environment parameters to flash storage ", SPIFLASH_CMDS);
+#endif
+
+/**
+ * Command "flash_show_env"
+ *
+ * Print environment parameters from flash storage
+ *
+ */
+#if (defined CSR_SPIFLASH_BASE && defined SPIFLASH_PAGE_SIZE && defined FLASH_SAVE_ENV)
+static void flash_show_env_handler(int nb_params, char **params)
+{
+	char flash_buffer[ENV_VAR_SIZE];
+	memset(flash_buffer, 0, sizeof(flash_buffer));
+	memcpy((void *)flash_buffer, (void *)FLASH_ENV_ADDRESS, ENV_VAR_SIZE);
+	if(nb_params == 0)
+	{
+		printf("Printing all environment variables...\n");
+		print_tokens(flash_buffer, NULL);
+	}
+	else if(nb_params == 1)
+	{
+		print_tokens(flash_buffer, params[0]);
+	}
+	else
+		printf("flash_show_env <token> ");
+}
+
+define_command(flash_show_env, flash_show_env_handler, "Print environment parameters from flash", SPIFLASH_CMDS);
+#endif
