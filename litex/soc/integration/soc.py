c80d5723c957f7b9f169c9b121891102eb231937
Florent Kermarrec <florent@enjoy-digital.fr>
2021-07-30 12:32:57 +0200

    soc/add_spi_flash: Reduce default_divisor.

--- litex/soc/integration/soc.py
+++ litex/soc/integration/soc.py
@@ -1497,7 +1497,7 @@ class LiteXSoC(SoC):
             self.check_if_exists(name + "_phy")
             self.check_if_exists(name + "_mmap")
             spiflash_pads   = self.platform.request(name if mode == "1x" else name + mode)
-            spiflash_phy    = LiteSPIPHY(spiflash_pads, module, default_divisor=max(int(self.sys_clk_freq/clk_freq), 2))
+            spiflash_phy    = LiteSPIPHY(spiflash_pads, module, default_divisor=int(self.sys_clk_freq/clk_freq))
             spiflash_core   = LiteSPI(spiflash_phy, clk_freq=clk_freq, mmap_endianness=self.cpu.endianness, **kwargs)
             setattr(self.submodules, name + "_phy",  spiflash_phy)
             setattr(self.submodules, name + "_core", spiflash_core)
