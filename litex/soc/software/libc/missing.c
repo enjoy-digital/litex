1a1a81bfa07d17e364db5cb35263ed1e5bbbb52d
Michal Sieron <msieron@internships.antmicro.com>
2021-08-11 18:06:18 +0200

    Move _exit function up, so gcc doesn't complain

--- litex/soc/software/libc/missing.c
+++ litex/soc/software/libc/missing.c
@@ -4,15 +4,16 @@ int getentropy(void *v, size_t s) {
     return -1;
 }
 
-int getpid() {
+int getpid(void) {
     return 1;
-}
-
-int kill(int pid, int name) {
-    _exit(0);
 }
 
 void _exit(int code) {
     while (1);
+}
+
+int kill(int pid, int name) {
+    _exit(0);
+    return 0;
 }
 
