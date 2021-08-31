837de615e6bd4ae3105472f3ee6c1feca02a78f2
Pawel Sagan <psagan@antmicro.com>
2021-08-31 16:39:34 +0200

    liblitespi: adjusting code to oddr/iddr litespi implementation Changing litespi registers configuration to be compatible with a new implementation. Signed-off-by: Paweł Sagan <psagan@antmicro.com>

--- litex/soc/software/liblitespi/spiflash.c
+++ litex/soc/software/liblitespi/spiflash.c
@@ -13,14 +13,13 @@
 
 #include "spiflash.h"
 
-#if defined(CSR_SPIFLASH_PHY_BASE) && defined(CSR_SPIFLASH_CORE_BASE)
+#if defined(CSR_SPIFLASH_CORE_BASE)
 
 //#define SPIFLASH_DEBUG
 //#define SPIFLASH_MODULE_DUMMY_BITS 8
 
 int spiflash_freq_init(void)
 {
-	unsigned int lowest_div = spiflash_phy_clk_divisor_read();
 	unsigned int crc = crc32((unsigned char *)SPIFLASH_BASE, SPI_FLASH_BLOCK_SIZE);
 	unsigned int crc_test = crc;
 
@@ -34,26 +33,14 @@ int spiflash_freq_init(void)
 		return -1;
 	}
 
-	while((crc == crc_test) && (lowest_div-- > 0)) {
-		spiflash_phy_clk_divisor_write((uint32_t)lowest_div);
-		crc_test = crc32((unsigned char *)SPIFLASH_BASE, SPI_FLASH_BLOCK_SIZE);
-#if SPIFLASH_DEBUG
-		printf("[DIV: %d] %08x\n\r", lowest_div, crc_test);
-#endif
-	}
-	lowest_div++;
-	printf("SPI Flash clk configured to %d MHz\n", (spiflash_core_sys_clk_freq_read()/(2*(1 + lowest_div)))/1000000);
-
-	spiflash_phy_clk_divisor_write(lowest_div);
-
+	printf("SPI Flash clk configured to %ld MHz\n", (unsigned long)(spiflash_frequency_read()/1e6));
 	return 0;
 }
-
 void spiflash_dummy_bits_setup(unsigned int dummy_bits)
 {
-	spiflash_phy_dummy_bits_write((uint32_t)dummy_bits);
+	spiflash_core_mmap_dummy_bits_write((uint32_t)dummy_bits);
 #if SPIFLASH_DEBUG
-	printf("Dummy bits set to: %d\n\r", spi_dummy_bits_read());
+	printf("Dummy bits set to: %d\n\r", spiflash_core_mmap_dummy_bits_read());
 #endif
 }
 
@@ -111,8 +98,6 @@ void spiflash_init(void)
 #ifndef SPIFLASH_SKIP_FREQ_INIT
 	/* Clk frequency auto-calibration. */
 	spiflash_freq_init();
-#else
-	printf("Warning: SPI Flash frequency auto-calibration skipped, using the default divisor of %d\n", spiflash_phy_clk_divisor_read());
 #endif
 
 	memspeed((unsigned int *) SPIFLASH_BASE, SPIFLASH_SIZE/16, 1, 0);
