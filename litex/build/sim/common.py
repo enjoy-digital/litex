5d9880888c303a846b9bd987ce97108de2c8c2d5
wuhanstudio <wuhanstudio@qq.com>
2021-09-07 11:18:37 +0100

    fix: missing colon syntax error

--- litex/build/sim/common.py
+++ litex/build/sim/common.py
@@ -14,7 +14,7 @@ class SimDDROutputImpl(Module):
             i_clk = clk
         )
 
-class SimDDROutput
+class SimDDROutput:
     @staticmethod
     def lower(dr):
         return SimDDROutputImpl(dr.o, dr.i1, dr.i2, dr.clk)
