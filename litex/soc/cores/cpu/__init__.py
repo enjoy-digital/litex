a489dadfbc2a63cd6db48db6d0112f4a1845dff3
Florent Kermarrec <florent@enjoy-digital.fr>
2021-10-12 09:02:48 +0200

    cpu/CPUNone: Add ethmac to mem_map as temporary build workaround for --cpu-type=None --with-ethernet.

--- litex/soc/cores/cpu/__init__.py
+++ litex/soc/cores/cpu/__init__.py
@@ -34,7 +34,7 @@ class CPUNone(CPU):
     io_regions           = {0x00000000: 0x100000000} # origin, length
     periph_buses         = []
     memory_buses         = []
-    mem_map              = {"csr": 0x00000000}
+    mem_map              = {"csr": 0x00000000, "ethmac": 0x00020000} # FIXME: remove ethmac.
 
 CPU_GCC_TRIPLE_RISCV32 = (
     "riscv64-unknown-elf",
