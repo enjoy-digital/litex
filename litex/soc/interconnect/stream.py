f224036138b1dbecba294b75378c949f4b5538a1
Florent Kermarrec <florent@enjoy-digital.fr>
2022-04-05 19:53:00 +0200

    stream/ClockDomainCrossing: Revert with_common_rst to False by default (Previous behavior).
    This seems to cause issues in simulation on some cores, this will first have to be fixed before
    using it as default. Cores requiring it will set it to True explicitly for now.

--- litex/soc/interconnect/stream.py
+++ litex/soc/interconnect/stream.py
@@ -242,7 +242,7 @@ class AsyncFIFO(_FIFOWrapper):
 # ClockDomainCrossing ------------------------------------------------------------------------------
 
 class ClockDomainCrossing(Module):
-    def __init__(self, layout, cd_from="sys", cd_to="sys", depth=None, with_common_rst=True):
+    def __init__(self, layout, cd_from="sys", cd_to="sys", depth=None, with_common_rst=False):
         self.sink   = Endpoint(layout)
         self.source = Endpoint(layout)
 
