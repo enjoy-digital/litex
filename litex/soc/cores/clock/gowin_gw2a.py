3acf22ff6fc18be3ca210972db6d34b840ca0d3a
Florent Kermarrec <florent@enjoy-digital.fr>
2022-06-03 12:04:11 +0200

    cores/clock/gowin_gw2a: Fix copyrights.

--- litex/soc/cores/clock/gowin_gw2a.py
+++ litex/soc/cores/clock/gowin_gw2a.py
@@ -1,6 +1,7 @@
 #
 # This file is part of LiteX.
 #
+# Copyright (c) 2022 Icenowy Zheng <icenowy@aosc.io>
 # Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
