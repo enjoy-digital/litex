6541a6c93b6daec3b47726ff117ddd08640a78d9
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2022-06-26 21:40:56 +0200

    build: gowin lattice/diamond lattice/oxide lattice/radiant microsemi quicklogic move to GenericToolchain

--- litex/build/gowin/gowin.py
+++ litex/build/gowin/gowin.py
@@ -13,85 +13,25 @@ from shutil import which, copyfile
 
 from migen.fhdl.structure import _Fragment
 
+from litex.build.generic_toolchain import GenericToolchain
 from litex.build.generic_platform import *
 from litex.build import tools
 
-# Constraints (.cst and .tcl) ----------------------------------------------------------------------
-
-def _build_cst(named_sc, named_pc):
-    cst = []
-
-    flat_sc = []
-    for name, pins, other, resource in named_sc:
-        if len(pins) > 1:
-            for i, p in enumerate(pins):
-                flat_sc.append((f"{name}[{i}]", p, other))
-        else:
-            flat_sc.append((name, pins[0], other))
-
-    for name, pin, other in flat_sc:
-        if pin != "X":
-            cst.append(f"IO_LOC \"{name}\" {pin};")
-
-        for c in other:
-            if isinstance(c, IOStandard):
-                cst.append(f"IO_PORT \"{name}\" IO_TYPE={c.name};")
-            elif isinstance(c, Misc):
-                cst.append(f"IO_PORT \"{name}\" {c.misc};")
-
-    if named_pc:
-        cst.extend(named_pc)
-
-    with open("top.cst", "w") as f:
-        f.write("\n".join(cst))
-
-def _build_sdc(clocks, vns):
-    sdc = []
-    for clk, period in sorted(clocks.items(), key=lambda x: x[0].duid):
-        sdc.append(f"create_clock -name {vns.get_name(clk)} -period {str(period)} [get_ports {{{vns.get_name(clk)}}}]")
-    with open("top.sdc", "w") as f:
-        f.write("\n".join(sdc))
-
-# Script -------------------------------------------------------------------------------------------
-
-def _build_tcl(name, partnumber, files, options):
-    tcl = []
-
-    # Set Device.
-    tcl.append(f"set_device -name {name} {partnumber}")
-
-    # Add IOs Constraints.
-    tcl.append("add_file top.cst")
-
-    # Add Timings Constraints.
-    tcl.append("add_file top.sdc")
-
-    # Add Sources.
-    for f, typ, lib in files:
-        # Support windows/powershell
-        if sys.platform == "win32": 
-            f = f.replace("\\", "\\\\")
-        tcl.append(f"add_file {f}")
-
-    # Set Options.
-    for opt, val in options.items():
-        tcl.append(f"set_option -{opt} {val}")
-
-    # Run.
-    tcl.append("run all")
-
-    # Generate .tcl.
-    with open("run.tcl", "w") as f:
-        f.write("\n".join(tcl))
 
 # GowinToolchain -----------------------------------------------------------------------------------
 
-class GowinToolchain:
+class GowinToolchain(GenericToolchain):
     attr_translate = {}
 
     def __init__(self):
+        super().__init__()
         self.options = {}
-        self.clocks  = dict()
+
+    def finalize(self):
+        if self.platform.verilog_include_paths:
+            self.options["include_path"] = "{" + ";".join(self.platform.verilog_include_paths) + "}"
+
+        self.apply_hyperram_integration_hack(self._build_name + ".v")
 
     def apply_hyperram_integration_hack(self, v_file):
         # FIXME: Gowin EDA expects a very specific HypeRAM integration pattern, modify generated verilog to match it.
@@ -116,85 +56,101 @@ class GowinToolchain:
         tools.replace_in_file(v_file, "[1:0] IO_psram_rwds,", "[1:0] IO_psram_rwds, /* synthesis syn_tristate = 1 */")
         tools.replace_in_file(v_file, "[15:0] IO_psram_dq,",    "[15:0] IO_psram_dq,  /* synthesis syn_tristate = 1 */")
 
-    def build(self, platform, fragment,
-        build_dir  = "build",
-        build_name = "top",
-        run        = True,
-        **kwargs):
+    def build(self, platform, fragment, **kwargs):
+        return self._build(platform, fragment, **kwargs)
 
-        # Create build directory.
-        cwd = os.getcwd()
-        os.makedirs(build_dir, exist_ok=True)
-        os.chdir(build_dir)
+    # Constraints (.cst ) --------------------------------------------------------------------------
 
-        # Finalize design
-        if not isinstance(fragment, _Fragment):
-            fragment = fragment.get_fragment()
-        platform.finalize(fragment)
+    def build_io_constraints(self):
+        cst = []
 
-        # Generate verilog
-        v_output = platform.get_verilog(fragment, name=build_name, **kwargs)
-        named_sc, named_pc = platform.resolve_signals(v_output.ns)
-        v_file = build_name + ".v"
-        v_output.write(v_file)
-        platform.add_source(v_file)
-        self.apply_hyperram_integration_hack(v_file)
+        flat_sc = []
+        for name, pins, other, resource in self.named_sc:
+            if len(pins) > 1:
+                for i, p in enumerate(pins):
+                    flat_sc.append((f"{name}[{i}]", p, other))
+            else:
+                flat_sc.append((name, pins[0], other))
 
-        if platform.verilog_include_paths:
-            self.options["include_path"] = "{" + ";".join(platform.verilog_include_paths) + "}"
+        for name, pin, other in flat_sc:
+            if pin != "X":
+                cst.append(f"IO_LOC \"{name}\" {pin};")
 
-        # Generate constraints file.
-        # IOs (.cst).
-        _build_cst(
-            named_sc = named_sc,
-            named_pc = named_pc
+            for c in other:
+                if isinstance(c, IOStandard):
+                    cst.append(f"IO_PORT \"{name}\" IO_TYPE={c.name};")
+                elif isinstance(c, Misc):
+                    cst.append(f"IO_PORT \"{name}\" {c.misc};")
+
+        if self.named_pc:
+            cst.extend(self.named_pc)
+
+        tools.write_to_file("top.cst", "\n".join(cst))
+        return ("top.cst", "CST")
+
+    # Timing Constraints (.sdc ) -------------------------------------------------------------------
+
+    def build_timing_constraints(self, vns):
+        sdc = []
+        for clk, period in sorted(self.clocks.items(), key=lambda x: x[0].duid):
+            sdc.append(f"create_clock -name {vns.get_name(clk)} -period {str(period)} [get_ports {{{vns.get_name(clk)}}}]")
+        tools.write_to_file("top.sdc", "\n".join(sdc))
+        return ("top.sdc", "SDC")
+
+    # Project (tcl) --------------------------------------------------------------------------------
+
+    def build_project(self):
+        tcl = []
+
+        # Set Device.
+        tcl.append(f"set_device -name {self.platform.devicename} {self.platform.device}")
+
+        # Add IOs Constraints.
+        tcl.append("add_file top.cst")
+
+        # Add Timings Constraints.
+        tcl.append("add_file top.sdc")
+
+        # Add Sources.
+        for f, typ, lib in self.platform.sources:
+            # Support windows/powershell
+            if sys.platform == "win32":
+                f = f.replace("\\", "\\\\")
+            tcl.append(f"add_file {f}")
+
+        # Set Options.
+        for opt, val in self.options.items():
+            tcl.append(f"set_option -{opt} {val}")
+
+        # Run.
+        tcl.append("run all")
+
+        # Generate .tcl.
+        tools.write_to_file("run.tcl", "\n".join(tcl))
+
+    # Script ---------------------------------------------------------------------------------------
+
+    def build_script(self):
+        return "" # gw_sh use
+
+    def run_script(self, script):
+        # Support Powershell/WSL platform
+        # Some python distros for windows (e.g, oss-cad-suite)
+        # which does not have 'os.uname' support, we should check 'sys.platform' firstly.
+        gw_sh = "gw_sh"
+        if sys.platform.find("linux") >= 0:
+            if os.uname().release.find("WSL") > 0:
+                gw_sh += ".exe"
+        if which(gw_sh) is None:
+            msg = "Unable to find Gowin toolchain, please:\n"
+            msg += "- Add Gowin toolchain to your $PATH."
+            raise OSError(msg)
+
+        if subprocess.call([gw_sh, "run.tcl"]) != 0:
+            raise OSError("Error occured during Gowin's script execution.")
+
+        # Copy Bitstream to from impl to gateware directory.
+        copyfile(
+            os.path.join("impl", "pnr", "project.fs"),
+            os.path.join(build_name + ".fs")
         )
-
-        # Timings (.sdc)
-        _build_sdc(
-            clocks  = self.clocks,
-            vns     = v_output.ns
-        )
-
-        # Generate build script (.tcl)
-        script = _build_tcl(
-            name       = platform.devicename,
-            partnumber = platform.device,
-            files      = platform.sources,
-            options    = self.options)
-
-        # Run
-        if run:
-            # Support Powershell/WSL platform
-            # Some python distros for windows (e.g, oss-cad-suite)
-            # which does not have 'os.uname' support, we should check 'sys.platform' firstly.
-            gw_sh = "gw_sh"
-            if sys.platform.find("linux") >= 0:
-                if os.uname().release.find("WSL") > 0:
-                    gw_sh += ".exe"
-            if which(gw_sh) is None:
-                msg = "Unable to find Gowin toolchain, please:\n"
-                msg += "- Add Gowin toolchain to your $PATH."
-                raise OSError(msg)
-
-            if subprocess.call([gw_sh, "run.tcl"]) != 0:
-                raise OSError("Error occured during Gowin's script execution.")
-
-            # Copy Bitstream to from impl to gateware directory.
-            copyfile(
-                os.path.join("impl", "pnr", "project.fs"),
-                os.path.join(build_name + ".fs")
-            )
-
-        os.chdir(cwd)
-
-        return v_output.ns
-
-    def add_period_constraint(self, platform, clk, period):
-        clk.attr.add("keep")
-        period = math.floor(period*1e3)/1e3 # round to lowest picosecond
-        if clk in self.clocks:
-            if period != self.clocks[clk]:
-                raise ValueError("Clock already constrained to {:.2f}ns, new constraint to {:.2f}ns"
-                    .format(self.clocks[clk], period))
-        self.clocks[clk] = period
--- litex/build/quicklogic/f4pga.py
+++ litex/build/quicklogic/f4pga.py
@@ -13,115 +13,85 @@ from shutil import which
 from migen.fhdl.structure import _Fragment
 
 from litex.build.generic_platform import *
+from litex.build.generic_toolchain import GenericToolchain
 from litex.build import tools
 from litex.build.quicklogic import common
-
-
-# IO Constraints (.pcf) ----------------------------------------------------------------------------
-def _format_io_pcf(signame, pin, others):
-    r = f"set_io {signame} {Pins(pin).identifiers[0]}\n"
-    return r
-
-def _build_io_pcf(named_sc, named_pc, build_name):
-    pcf = ""
-    for sig, pins, others, resname in named_sc:
-        if len(pins) > 1:
-            for i, p in enumerate(pins):
-                pcf += _format_io_pcf(sig + "(" + str(i) + ")", p, others)
-        else:
-            pcf += _format_io_pcf(sig, pins[0], others)
-    tools.write_to_file(build_name + ".pcf", pcf)
-
-# Build Makefile -----------------------------------------------------------------------------------
-
-def _build_makefile(platform, sources, build_dir, build_name):
-    makefile = []
-
-    # Define Paths.
-    makefile.append("mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))")
-    makefile.append("current_dir := $(patsubst %/,%,$(dir $(mkfile_path)))")
-    # bit -> h and bit -> bin requires TOP_F
-    makefile.append(f"TOP_F={build_name}")
-
-    # Create Project.
-    # FIXME: Only use top file for now and ignore .init files.
-    makefile.append("all: {top}_bit.h {top}.bin build/{top}.bit".format(top=build_name))
-    # build bit file (default)
-    makefile.append(f"build/{build_name}.bit:")
-    makefile.append("\tql_symbiflow -compile -d {device} -P {part} -v {verilog} -t {top} -p {pcf}".format(
-        device  = platform.device,
-        part    = {"ql-eos-s3": "PU64"}.get(platform.device),
-        verilog = f"{build_name}.v",
-        top     = build_name,
-        pcf     = f"{build_name}.pcf"
-    ))
-    # build header to include in CPU firmware
-    makefile.append("{top}_bit.h: build/{top}.bit".format(top=build_name))
-    makefile.append(f"\t(cd build; TOP_F=$(TOP_F) symbiflow_write_bitheader)")
-    # build binary to write in dedicated FLASH area
-    makefile.append("{top}.bin: build/{top}.bit".format(top=build_name))
-    makefile.append(f"\t(cd build; TOP_F=$(TOP_F) symbiflow_write_binary)")
-
-    # Generate Makefile.
-    tools.write_to_file("Makefile", "\n".join(makefile))
-
-def _run_make():
-    make_cmd = ["make", "-j1"]
-
-    if which("ql_symbiflow") is None:
-        msg = "Unable to find QuickLogic Symbiflow toolchain, please:\n"
-        msg += "- Add QuickLogic Symbiflow toolchain to your $PATH."
-        raise OSError(msg)
-
-    if subprocess.call(make_cmd) != 0:
-        raise OSError("Error occured during QuickLogic Symbiflow's script execution.")
 
 
 # F4PGAToolchain -------------------------------------------------------------------------------
 # Formerly SymbiflowToolchain, Symbiflow has been renamed to F4PGA -----------------------------
 
-class F4PGAToolchain:
+class F4PGAToolchain(GenericToolchain):
     attr_translate = {}
 
     special_overrides = common.quicklogic_special_overrides
 
     def __init__(self):
-        self.clocks      = dict()
-        self.false_paths = set()
+        super().__init__()
 
-    def build(self, platform, fragment,
-            build_dir  = "build",
-            build_name = "top",
-            run        = False,
-            **kwargs):
+    def build(self, platform, fragment, **kwargs):
+        return self._build(platform, fragment, **kwargs)
 
-        # Create build directory.
-        os.makedirs(build_dir, exist_ok=True)
-        cwd = os.getcwd()
-        os.chdir(build_dir)
+    # IO Constraints (.pcf) ------------------------------------------------------------------------
 
-        # Finalize design.
-        if not isinstance(fragment, _Fragment):
-            fragment = fragment.get_fragment()
-        platform.finalize(fragment)
+    @classmethod
+    def _format_io_pcf(cls, signame, pin, others):
+        r = f"set_io {signame} {Pins(pin).identifiers[0]}\n"
+        return r
 
-        # Generate verilog.
-        v_output = platform.get_verilog(fragment, name=build_name, **kwargs)
-        named_sc, named_pc = platform.resolve_signals(v_output.ns)
-        top_file = build_name + ".v"
-        v_output.write(top_file)
-        platform.add_source(top_file)
+    def build_io_constraints(self):
+        pcf = ""
+        for sig, pins, others, resname in self.named_sc:
+            if len(pins) > 1:
+                for i, p in enumerate(pins):
+                    pcf += self._format_io_pcf(sig + "(" + str(i) + ")", p, others)
+            else:
+                pcf += self._format_io_pcf(sig, pins[0], others)
+        tools.write_to_file(self._build_name + ".pcf", pcf)
+        return (self._build_name + ".pcf", "PCF")
 
-        # Generate .pcf IO constraints file.
-        _build_io_pcf(named_sc, named_pc, build_name)
+    # Build Makefile -------------------------------------------------------------------------------
 
-        # Generate Makefie.
-        _build_makefile(platform, platform.sources, build_dir, build_name)
+    def build_script(self):
+        makefile = []
 
-        # Run.
-        if run:
-            _run_make()
+        # Define Paths.
+        makefile.append("mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))")
+        makefile.append("current_dir := $(patsubst %/,%,$(dir $(mkfile_path)))")
+        # bit -> h and bit -> bin requires TOP_F
+        makefile.append(f"TOP_F={self._build_name}")
 
-        os.chdir(cwd)
+        # Create Project.
+        # FIXME: Only use top file for now and ignore .init files.
+        makefile.append("all: {top}_bit.h {top}.bin build/{top}.bit".format(top=self._build_name))
+        # build bit file (default)
+        makefile.append(f"build/{self._build_name}.bit:")
+        makefile.append("\tql_symbiflow -compile -d {device} -P {part} -v {verilog} -t {top} -p {pcf}".format(
+            device  = self.platform.device,
+            part    = {"ql-eos-s3": "PU64"}.get(self.platform.device),
+            verilog = f"{self._build_name}.v",
+            top     = self._build_name,
+            pcf     = f"{self._build_name}.pcf"
+        ))
+        # build header to include in CPU firmware
+        makefile.append("{top}_bit.h: build/{top}.bit".format(top=self._build_name))
+        makefile.append(f"\t(cd build; TOP_F=$(TOP_F) symbiflow_write_bitheader)")
+        # build binary to write in dedicated FLASH area
+        makefile.append("{top}.bin: build/{top}.bit".format(top=self._build_name))
+        makefile.append(f"\t(cd build; TOP_F=$(TOP_F) symbiflow_write_binary)")
 
-        return v_output.ns
+        # Generate Makefile.
+        tools.write_to_file("Makefile", "\n".join(makefile))
+
+        return "Makefile"
+
+    def run_script(self, script):
+        make_cmd = ["make", "-j1"]
+
+        if which("ql_symbiflow") is None:
+            msg = "Unable to find QuickLogic Symbiflow toolchain, please:\n"
+            msg += "- Add QuickLogic Symbiflow toolchain to your $PATH."
+            raise OSError(msg)
+
+        if subprocess.call(make_cmd) != 0:
+            raise OSError("Error occured during QuickLogic Symbiflow's script execution.")
