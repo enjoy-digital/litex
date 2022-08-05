ae8deda1865c48c696d2c62ae4bb12ac2e3d7fd9
Florent Kermarrec <florent@enjoy-digital.fr>
2022-08-05 11:20:25 +0200

    interconnect/axi/AXIArbiter: valid also needs to be filtered.
    Fixes un-sollicited valids on masters.

--- litex/soc/interconnect/axi/axi_lite.py
+++ litex/soc/interconnect/axi/axi_lite.py
@@ -603,7 +603,7 @@ class AXILiteArbiter(Module):
     """
     def __init__(self, masters, target):
         self.submodules.rr_write = roundrobin.RoundRobin(len(masters), roundrobin.SP_CE)
-        self.submodules.rr_read = roundrobin.RoundRobin(len(masters), roundrobin.SP_CE)
+        self.submodules.rr_read  = roundrobin.RoundRobin(len(masters), roundrobin.SP_CE)
 
         def get_sig(interface, channel, name):
             return getattr(getattr(interface, channel), name)
@@ -622,8 +622,8 @@ class AXILiteArbiter(Module):
                 source = get_sig(target, channel, name)
                 for i, m in enumerate(masters):
                     dest = get_sig(m, channel, name)
-                    if name == "ready":
-                        self.comb += dest.eq(source & (rr.grant == i))
+                    if name in ["valid", "ready"]:
+                        self.comb += If(rr.grant == i, dest.eq(source))
                     else:
                         self.comb += dest.eq(source)
 
