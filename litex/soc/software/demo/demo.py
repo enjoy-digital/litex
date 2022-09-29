af582372030db380e570e17c20a53e4d0356b3b5
Florent Kermarrec <florent@enjoy-digital.fr>
2022-09-29 17:29:53 +0200

    software/demo: Add comments for Nix specific changes (To ease future maintenance and avoid breaking it).

--- litex/soc/software/demo/demo.py
+++ litex/soc/software/demo/demo.py
@@ -24,7 +24,7 @@ def main():
 
     # Copy contents to demo directory
     os.system(f"cp {os.path.abspath(os.path.dirname(__file__))}/* demo")
-    os.system("chmod -R u+w demo")
+    os.system("chmod -R u+w demo") # Nix specific: Allow linker script to be modified.
 
     # Update memory region.
     replace_in_file("demo/linker.ld", "main_ram", args.mem)
@@ -37,7 +37,8 @@ def main():
     os.system("cp demo/demo.bin ./")
 
     # Prepare flash boot image.
-    os.system(f"{sys.executable or 'python3'} -m litex.soc.software.crcfbigen demo.bin -o demo.fbi --fbi --little") # FIXME: Endianness.
+    python3 = sys.executable or "python3" # Nix specific: Reuse current Python executable if available.
+    os.system(f"{python3} -m litex.soc.software.crcfbigen demo.bin -o demo.fbi --fbi --little") # FIXME: Endianness.
 
 if __name__ == "__main__":
     main()
