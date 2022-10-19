519b411954fe57cb38776c90f4fc5cb7e8e2fe13
Florent Kermarrec <florent@enjoy-digital.fr>
2022-10-19 11:40:51 +0200

    core/spi/spi_bone: Update header.

--- litex/soc/cores/spi/spi_bone.py
+++ litex/soc/cores/spi/spi_bone.py
@@ -2,6 +2,7 @@
 # This file is part of LiteX.
 #
 # Copyright (c) 2019 Sean Cross <sean@xobs.io>
+# Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
