3922359ba17652149781079e5ff2b4296ec2b82f
Joel Stanley <joel@jms.id.au>
2023-02-27 12:55:50 +1030

    test: Reinstate microwatt and neorv32
    They appear to be passing CI again.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- test/test_cpu.py
+++ test/test_cpu.py
@@ -45,10 +45,10 @@ class TestCPU(unittest.TestCase):
             "serv",         # (riscv   / softcore)
             "vexriscv",     # (riscv   / softcore)
             "vexriscv_smp", # (riscv   / softcore)
+            "microwatt",    # (ppc64   / softcore)
+            "neorv32",      # (riscv   / softcore)
         ]
         untested_cpus = [
-            "microwatt",    # (ppc64   / softcore) -> Broken 2022.01.12 -> https://github.com/enjoy-digital/litex/actions/runs/3900056883/jobs/6662146988
-            "neorv32",      # (riscv   / softcore) -> Broken 2022.01.20 -> https://github.com/enjoy-digital/litex/actions/runs/3970180588/jobs/6805602574
             "blackparrot",  # (riscv   / softcore) -> Broken install?
             "cortex_m1",    # (arm     / softcore) -> Proprietary code.
             "cortex_m3",    # (arm     / softcore) -> Proprieraty code.
