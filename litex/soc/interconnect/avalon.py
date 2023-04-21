035becf229404206fcf79c69a01c260cb021d238
Hans Baier <hansfbaier@gmail.com>
2023-04-21 19:14:14 +0700

    basic AvalonMM interface implementation

--- litex/soc/interconnect/avalon.py
+++ litex/soc/interconnect/avalon.py
@@ -10,6 +10,119 @@ from migen import *
 
 from litex.soc.interconnect import stream
 
+_layout = [
+    ("address",          "adr_width", DIR_M_TO_S),
+    ("writedata",       "data_width", DIR_M_TO_S),
+    ("readdata",        "data_width", DIR_S_TO_M),
+    ("readdatavalid",              1, DIR_S_TO_M),
+    ("byteenable",       "sel_width", DIR_M_TO_S),
+    ("read",                       1, DIR_M_TO_S),
+    ("write",                      1, DIR_M_TO_S),
+    ("waitrequest",                1, DIR_S_TO_M),
+    ("burstbegin",                 1, DIR_M_TO_S), # this is optional
+    ("burstcount",                 8, DIR_M_TO_S),
+    ("chipselect",                 1, DIR_M_TO_S), # this is optional
+]
+
+class MMInterface(Record):
+    def __init__(self, data_width=32, adr_width=30, bursting=False, **kwargs):
+        self.data_width = data_width
+        self.bursting   = bursting
+        if kwargs.get("adr_width", False):
+            adr_width = kwargs["adr_width"] - int(log2(data_width//8))
+        self.adr_width = adr_width
+        Record.__init__(self, set_layout_parameters(_layout,
+            adr_width  = adr_width,
+            data_width = data_width,
+            sel_width  = data_width//8))
+        self.address.reset_less    = True
+        self.writedata.reset_less  = True
+        self.readdata.reset_less   = True
+        self.byteenable.reset_less = True
+
+    @staticmethod
+    def like(other):
+        return Interface(len(other.writedata))
+
+    def get_ios(self, bus_name="avl"):
+        subsignals = []
+        for name, width, direction in self.layout:
+            subsignals.append(Subsignal(name, Pins(width)))
+        ios = [(bus_name , 0) + tuple(subsignals)]
+        return ios
+
+    def connect_to_pads(self, pads, mode="master"):
+        assert mode in ["slave", "master"]
+        r = []
+        for name, width, direction in self.layout:
+            sig  = getattr(self, name)
+            pad  = getattr(pads, name)
+            if mode == "master":
+                if direction == DIR_M_TO_S:
+                    r.append(pad.eq(sig))
+                else:
+                    r.append(sig.eq(pad))
+            else:
+                if direction == DIR_S_TO_M:
+                    r.append(pad.eq(sig))
+                else:
+                    r.append(sig.eq(pad))
+        return r
+
+    def read(self, address, byteenable=None, burstcount=None, chipselect=None):
+        if byteenable is None:
+            byteenable = 2**len(self.byteenable) - 1
+        yield self.address.eq(address)
+        yield self.write.eq(0)
+        yield self.read.eq(1)
+        yield self.byteenable.eq(byteenable)
+        if burstcount is not None:
+            yield self.burstcount.eq(burstcount)
+        if chipselect is not None:
+            yield self.chipselect.eq(chipselect)
+        yield
+        while (yield self.waitrequest):
+            yield
+        yield self.read.eq(0)
+        # actually don't care outside of a transaction
+        # this makes the traces look neater
+        yield self.byteenable.eq(0)
+        if burstcount is not None:
+            yield self.burstcount.eq(0)
+        if chipselect is not None:
+            yield self.chipselect.eq(0)
+        while not (yield self.readdatavalid):
+            yield
+        return (yield self.readdata)
+
+    def write(self, address, writedata, byteenable=None, chipselect=None):
+        if not isinstance(writedata, list):
+            writedata = [ writedata ]
+        burstcount = len(writedata)
+        if byteenable is None:
+            byteenable = 2**len(self.byteenable) - 1
+        yield self.address.eq(address)
+        yield self.write.eq(1)
+        yield self.read.eq(0)
+        yield self.byteenable.eq(byteenable)
+        if burstcount is not None:
+            yield self.burstcount.eq(burstcount)
+        if chipselect is not None:
+            yield self.chipselect.eq(chipselect)
+        for data in writedata:
+            yield self.writedata.eq(data)
+            yield
+            while (yield self.waitrequest):
+                yield
+            yield self.burstcount.eq(0)
+        yield self.writedata.eq(0)
+        yield self.write.eq(0)
+        # actually don't care outside of a transaction
+        # this makes the traces look neater
+        yield self.byteenable.eq(0)
+        if chipselect is not None:
+            yield self.chipselect.eq(0)
+
 # Avalon-ST to/from native LiteX's stream ----------------------------------------------------------
 
 # In native LiteX's streams, ready signal has no latency (similar to AXI). In Avalon-ST streams the
