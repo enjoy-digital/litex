7d323fd30a7f19bfafc95d391f7b750f0086e629
Hans Baier <hansfbaier@gmail.com>
2023-04-24 18:03:30 +0700

    fix copyright

--- test/test_avalon.py
+++ test/test_avalon.py
@@ -1,7 +1,6 @@
 #
 # This file is part of LiteX.
 #
-# Copyright (c) 2019 Florent Kermarrec <florent@enjoy-digital.fr>
 # Copyright (c) 2023 Hans Baier <hansfbaier@gmail.com>
 # SPDX-License-Identifier: BSD-2-Clause
 
