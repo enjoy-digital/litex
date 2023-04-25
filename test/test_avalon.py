cadc76c9067ec3f3f8a6be7ccb90110df0074225
Hans Baier <hansfbaier@gmail.com>
2023-04-25 09:22:57 +0700

    Avalon2Wishbone test: add non burst read check also

--- test/test_avalon.py
+++ test/test_avalon.py
@@ -49,6 +49,11 @@ class TestAvalon2Wishbone(unittest.TestCase):
             self.assertEqual((yield from dut.avl.continue_read_burst()), 0xc0ffee00)
             self.assertEqual((yield from dut.avl.continue_read_burst()), 0x76543210)
             yield
+            self.assertEqual((yield from dut.avl.bus_read(0x0000)), 0x01234567)
+            self.assertEqual((yield from dut.avl.bus_read(0x0004)), 0x89abcdef)
+            self.assertEqual((yield from dut.avl.bus_read(0x0008)), 0xdeadbeef)
+            self.assertEqual((yield from dut.avl.bus_read(0x000c)), 0xc0ffee00)
+            self.assertEqual((yield from dut.avl.bus_read(0x0010)), 0x76543210)
             yield
             yield
 
