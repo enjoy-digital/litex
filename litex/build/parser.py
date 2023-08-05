ef778613d39762c837b4185973ec100336e67f9f
Andrew Dennison <andrew@motec.com.au>
2023-08-05 17:48:59 +1000

    build/parser: add optional soc and include args in soc_argdict
    if soc is set this automatically passes arguments to the soc
    in soc_argdict to simplify board platforms. Example usage below:
    
        # IOs ----------------------------------------------------------------------------------------------
    
        _io = [
            # Clk / Rst.
            ("sys_clk", 0, Pins(1)),
            ("sys_rst", 0, Pins(1)),
        ]
    
        # Platform -----------------------------------------------------------------------------------------
    
        class Platform(SimPlatform):
            name = "sim"
            def __init__(self):
                SimPlatform.__init__(self, "SIM", _io)
    
        # MySimSoC ----------------------------------------------------------------------------------------
    
        class MySimSoC(SoCCore):
            def __init__(self, sys_clk_freq, etherbone, etherbone_mac_address, etherbone_ip_address, **kwargs):
                # etc
    
        # Build --------------------------------------------------------------------------------------------
    
        def main():
            from litex.build.parser import LiteXArgumentParser
            bool_action = argparse.BooleanOptionalAction
            parser = LiteXArgumentParser(platform=Platform, soc=MySimSoC, description="My simulation.")
    
            # NOTE: all target arguments have a default here, not in soc.__init__(), so they are documented.
            parser.add_target_argument("--sys-clk-freq", default=100e6, type=float, help="System clock frequency.")
            parser.add_target_argument("--etherbone", default=False, action=bool_action,  help="Include Etherbone support.")
            parser.add_target_argument("--etherbone-mac-address", default=0x10e2d5000000, help="Etherbone MAC address.")
            parser.add_target_argument("--etherbone-ip-address", default="192.168.1.50", help="Etherbone IP address.")
    
            # override some defaults
            parser.set_defaults(trace=True)
            parser.set_defaults(trace_fst=True)
            parser.set_defaults(timer_uptime=True)
            parser.set_defaults(integrated_rom_size=0x10000)
            parser.set_defaults(integrated_main_ram_size=0x10000)
            parser.set_defaults(uart_name="sim") # "crossover" for terminal via etherbone
            parser.set_defaults(soc_csv="scr.csv")
            args = parser.parse_args()
    
            # etc
    
            # Create SoC.
            soc = MySimSoC(**parser.soc_argdict) # args are automatically passed

--- litex/build/parser.py
+++ litex/build/parser.py
@@ -9,6 +9,7 @@ import sys
 import logging
 import argparse
 import importlib
+import inspect
 
 from litex.soc.cores import cpu
 from litex.soc.integration import soc_core
@@ -36,7 +37,7 @@ class LiteXArgumentParser(argparse.ArgumentParser):
         couple argument name / default value to apply just before to call
         parse_args()
     """
-    def __init__(self, platform=None, **kwargs):
+    def __init__(self, platform=None, soc=None, **kwargs):
         """
         CTOR: create a Target options group, adds toolchain, build and load
         arguments. Call builder_args and soc_core_args for fill parser with
@@ -51,6 +52,7 @@ class LiteXArgumentParser(argparse.ArgumentParser):
         """
         argparse.ArgumentParser.__init__(self, kwargs)
         self._platform          = None
+        self._soc               = soc
         self._device            = None
         self.toolchains         = None
         self._default_toolchain = None
@@ -145,9 +147,20 @@ class LiteXArgumentParser(argparse.ArgumentParser):
 
         Return
         ======
-        soc_core arguments dict
+        soc_core arguments dict plus soc args when soc has been supplied.
         """
-        return soc_core.soc_core_argdict(self._args) # FIXME: Rename to soc_argdict in the future.
+        argdict = soc_core.soc_core_argdict(self._args) # FIXME: Rename to soc_argdict in the future.
+
+        # when _soc is set, iterate on _soc args adding supplied args to argdict.
+        if self._soc:
+            for a in inspect.getfullargspec(self._soc.__init__).args:
+                arg = getattr(self._args, a, None)
+                # Fill Dict.
+                if arg is not None:
+                    argdict[a] = arg
+                    print(f"arg {a}={arg}")
+
+        return argdict
 
     @property
     def toolchain_argdict(self):
