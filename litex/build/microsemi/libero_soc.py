45fa51025b5e9cbbe1906acfeb22805f05cefacb
Ciaran Lappin <ciaran.lappin@microchip.com>
2023-12-21 10:53:23 +0000

    Adding tcl command to export_prog_job file for FPExpress, saves in Libero SoC default location.

--- litex/build/microsemi/libero_soc.py
+++ litex/build/microsemi/libero_soc.py
@@ -163,6 +163,23 @@ class MicrosemiLiberoSoCPolarfireToolchain(GenericToolchain):
         tcl.append("run_tool -name {PLACEROUTE}")
         tcl.append("run_tool -name {GENERATEPROGRAMMINGDATA}")
         tcl.append("run_tool -name {GENERATEPROGRAMMINGFILE}")
+        
+        # Exort the FPExpress programming file to Libero SoC default location
+        tcl.append(" export_prog_job \
+         -job_file_name {top} \
+         -export_dir {./impl/designer/top/export} \
+         -bitstream_file_type {TRUSTED_FACILITY} \
+         -bitstream_file_components {FABRIC SNVM} \
+         -zeroization_likenew_action 0 \
+         -zeroization_unrecoverable_action 0 \
+         -program_design 1 \
+         -program_spi_flash 0 \
+         -include_plaintext_passkey 0 \
+         -design_bitstream_format {PPD} \
+         -prog_optional_procedures {} \
+         -skip_recommended_procedures {} \
+         -sanitize_snvm 0 ")
+        
 
         # Generate tcl
         tools.write_to_file(self._build_name + ".tcl", "\n".join(tcl))
