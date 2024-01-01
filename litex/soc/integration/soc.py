enjoy-digital/litex

commit 002aad7a43c0a146914375f5ea8c361ce8ad16c8

soc/test: Make data_width/address_width/addressing explicit on Wishbone.Interface calls.

litex/soc/integration/soc.py
@@ -1627,7 +1627,9 @@ class LiteXSoC(SoC):
                     else:
                         mem_wb  = wishbone.Interface(
                             data_width = self.cpu.mem_axi.data_width,
-                            adr_width  = 32-log2_int(mem_bus.data_width//8))
+                            adr_width  = 32-log2_int(mem_bus.data_width//8,
+                            addressing = "word",
+                        ))
                         mem_a2w = axi.AXI2Wishbone(
                             axi          = mem_bus,
                             wishbone     = mem_wb,
@@ -1664,7 +1666,7 @@ class LiteXSoC(SoC):
             port.data_width = 2**int(log2(port.data_width)) # Round to nearest power of 2.
 
             # Create Wishbone Slave.
-            wb_sdram = wishbone.Interface(data_width=self.bus.data_width)
+            wb_sdram = wishbone.Interface(data_width=self.bus.data_width, address_width=32, addressing="word")
             self.bus.add_slave(name="main_ram", slave=wb_sdram)
 
             # L2 Cache
@@ -1676,7 +1678,7 @@ class LiteXSoC(SoC):
                 l2_cache = wishbone.Cache(
                     cachesize = l2_cache_size//4,
                     master    = wb_sdram,
-                    slave     = wishbone.Interface(l2_cache_data_width),
+                    slave     = wishbone.Interface(data_width=l2_cache_data_width, address_width=32, addressing="word"),
                     reverse   = l2_cache_reverse)
                 if l2_cache_full_memory_we:
                     l2_cache = FullMemoryWE()(l2_cache)
@@ -1684,7 +1686,7 @@ class LiteXSoC(SoC):
                 litedram_wb = self.l2_cache.slave
                 self.add_config("L2_SIZE", l2_cache_size)
             else:
-                litedram_wb = wishbone.Interface(port.data_width)
+                litedram_wb = wishbone.Interface(data_width=port.data_width, address_width=32, addressing="word")
                 self.submodules += wishbone.Converter(wb_sdram, litedram_wb)
 
             # Wishbone Slave <--> LiteDRAM bridge.
@@ -1926,7 +1928,11 @@ class LiteXSoC(SoC):
         # Block2Mem DMA.
         if "read" in mode:
             self.check_if_exists(f"{name}_block2mem")
-            bus = wishbone.Interface(data_width=self.bus.data_width, adr_width=self.bus.get_address_width(standard="wishbone"))
+            bus = wishbone.Interface(
+                data_width = self.bus.data_width,
+                adr_width  = self.bus.get_address_width(standard="wishbone"),
+                addressing = "word",
+            )
             sdcard_block2mem = SDBlock2MemDMA(bus=bus, endianness=self.cpu.endianness)
             self.add_module(name=f"{name}_block2mem", module=sdcard_block2mem)
             self.comb += sdcard_core.source.connect(sdcard_block2mem.sink)
@@ -1936,7 +1942,11 @@ class LiteXSoC(SoC):
         # Mem2Block DMA.
         if "write" in mode:
             self.check_if_exists(f"{name}_mem2block")
-            bus = wishbone.Interface(data_width=self.bus.data_width, adr_width=self.bus.get_address_width(standard="wishbone"))
+            bus = wishbone.Interface(
+                data_width = self.bus.data_width,
+                adr_width  = self.bus.get_address_width(standard="wishbone"),
+                addressing = "word",
+            )
             sdcard_mem2block = SDMem2BlockDMA(bus=bus, endianness=self.cpu.endianness)
             self.add_module(name=f"{name}_mem2block", module=sdcard_mem2block)
             self.comb += sdcard_mem2block.source.connect(sdcard_core.sink)
@@ -2007,11 +2017,16 @@ class LiteXSoC(SoC):
         # Sector2Mem DMA.
         if "read" in mode:
             self.check_if_exists(f"{name}_sector2mem")
-            bus = wishbone.Interface(data_width=self.bus.data_width, adr_width=self.bus.get_address_width(standard="wishbone"))
+            bus = wishbone.Interface(
+                data_width = self.bus.data_width,
+                adr_width  = self.bus.get_address_width(standard="wishbone"),
+                addressing = "word",
+            )
             sata_sector2mem = LiteSATASector2MemDMA(
                port       = sata_crossbar.get_port(),
                bus        = bus,
-               endianness = self.cpu.endianness)
+               endianness = self.cpu.endianness,
+            )
             self.add_module(name=f"{name}_sector2mem", module=sata_sector2mem)
             dma_bus = getattr(self, "dma_bus", self.bus)
             dma_bus.add_master(name=f"{name}_sector2mem", master=bus)
@@ -2019,11 +2034,16 @@ class LiteXSoC(SoC):
         # Mem2Sector DMA.
         if "write" in mode:
             self.check_if_exists(f"{name}_mem2sector")
-            bus = wishbone.Interface(data_width=self.bus.data_width, adr_width=self.bus.get_address_width(standard="wishbone"))
+            bus = wishbone.Interface(
+                data_width = self.bus.data_width,
+                adr_width  = self.bus.get_address_width(standard="wishbone"),
+                addressing = "word",
+            )
             sata_mem2sector = LiteSATAMem2SectorDMA(
                bus        = bus,
                port       = sata_crossbar.get_port(),
-               endianness = self.cpu.endianness)
+               endianness = self.cpu.endianness,
+            )
             self.add_module(name=f"{name}_mem2sector", module=sata_mem2sector)
             dma_bus = getattr(self, "dma_bus", self.bus)
             dma_bus.add_master(name=f"{name}_mem2sector", master=bus)

litex/soc/cores/cpu/cv32e40p/core.py
@@ -186,7 +186,7 @@ class Wishbone2APB(LiteXModule):
 
 class TraceCollector(LiteXModule):
     def __init__(self, trace_depth=16384):
-        self.bus  = bus  = wishbone.Interface()
+        self.bus  = bus  = wishbone.Interface(data_width=32, address_width=32, addressing="word")
         self.sink = sink = stream.Endpoint([("data", 32)])
 
         clear   = Signal()

litex/soc/cores/emif.py
@@ -18,7 +18,7 @@ class EMIF(LiteXModule):
     Provides a simple EMIF to Wishbone Master bridge.
     """
     def __init__(self, pads):
-        self.bus = bus = wishbone.Interface()
+        self.bus = bus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
 
         # # #
 
@@ -87,7 +87,7 @@ class EMIF(LiteXModule):
 
 class EMIF16To32Adapter(LiteXModule):
     def __init__(self, emif):
-        self.bus = bus = wishbone.Interface()
+        self.bus = bus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
 
         # # #
 

litex/soc/cores/hyperbus.py
@@ -29,7 +29,7 @@ class HyperRAM(LiteXModule):
     """
     def __init__(self, pads, latency=6, sys_clk_freq=None):
         self.pads = pads
-        self.bus  = bus = wishbone.Interface()
+        self.bus  = bus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
 
         # # #
 

litex/soc/cores/i2s.py
@@ -163,7 +163,7 @@ class S7I2S(LiteXModule):
         self.comb += [rising_edge.eq(clk_pin & ~clk_d), falling_edge.eq(~clk_pin & clk_d)]
 
         # Wishbone bus
-        self.bus = bus = wishbone.Interface()
+        self.bus = bus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
         rd_ack = Signal()
         wr_ack = Signal()
         self.comb += [

litex/soc/cores/led.py
@@ -134,7 +134,7 @@ class WS2812(LiteXModule):
     """
     def __init__(self, pad, nleds, sys_clk_freq, bus_mastering=False, bus_base=None, revision="new", init=None):
         if bus_mastering:
-            self.bus  = bus = wishbone.Interface(data_width=32)
+            self.bus  = bus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
         else:
             # Memory.
             mem = Memory(32, nleds, init=init)
@@ -145,7 +145,7 @@ class WS2812(LiteXModule):
             self.wb_mem = wishbone.SRAM(
                 mem_or_size = mem,
                 read_only   = False,
-                bus         = wishbone.Interface(data_width=32)
+                bus         = wishbone.Interface(data_width=32, address_width=32, addressing="word")
             )
             self.bus = self.wb_mem.bus
 

litex/soc/cores/ram/lattice_ice40.py
@@ -27,7 +27,7 @@ of 4 SPRAMs for this, so the only other valid config is using all 4 SPRAMs by de
 
 class Up5kSPRAM(LiteXModule):
     def __init__(self, width=32, size=64*kB):
-        self.bus = wishbone.Interface(width)
+        self.bus = wishbone.Interface(data_width=width, address_width=32, addressing="word")
 
         # # #
 

litex/soc/cores/ram/lattice_nx.py
@@ -53,7 +53,7 @@ def initval_parameters(contents, width):
 
 class NXLRAM(LiteXModule):
     def __init__(self, width=32, size=128*kB, init=[]):
-        self.bus = wishbone.Interface(width)
+        self.bus = wishbone.Interface(data_width=width, address_width=32, addressing="word")
         assert width in [32, 64]
         self.width = width
         self.size = size

commit 61ba0d99dc68392a16d0409ee13322cb4b5374fc

soc: fix typo in cpu mem_bus axi-via-wb downconvert
Fixes: 002aad7a4

Signed-off-by: Gabriel Somlo <gsomlo@gmail.com>

litex/soc/integration/soc.py
@@ -1651,9 +1651,9 @@ class LiteXSoC(SoC):
                     else:
                         mem_wb  = wishbone.Interface(
                             data_width = self.cpu.mem_axi.data_width,
-                            adr_width  = 32-log2_int(mem_bus.data_width//8,
+                            adr_width  = 32-log2_int(mem_bus.data_width//8),
                             addressing = "word",
-                        ))
+                        )
                         mem_a2w = axi.AXI2Wishbone(
                             axi          = mem_bus,
                             wishbone     = mem_wb,