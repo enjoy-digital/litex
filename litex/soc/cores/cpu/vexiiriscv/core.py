7b7334fc1799c64f8dd5e195a1b827607035a959
Dolu1990 <charles.papon.90@gmail.com>
2024-05-14 11:41:21 +0200

    cpu/vexiiriscv update

--- litex/soc/cores/cpu/vexiiriscv/core.py
+++ litex/soc/cores/cpu/vexiiriscv/core.py
@@ -130,7 +130,7 @@ class VexiiRiscv(CPU):
         print(args)
 
         if args.update_repo != "no":
-           NaxRiscv.git_setup("VexiiRiscv", ndir, "https://github.com/SpinalHDL/VexiiRiscv.git", "fpu_internal", "b4e86579" if args.update_repo=="recommended" else None)
+           NaxRiscv.git_setup("VexiiRiscv", ndir, "https://github.com/SpinalHDL/VexiiRiscv.git", "fpu_internal", "c330b794" if args.update_repo=="recommended" else None)
 
 
 
