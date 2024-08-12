e0968b357442a4066b0159d9a826d8cb0f75ce0d
Mai-Lapyst <67418776+Mai-Lapyst@users.noreply.github.com>
2024-08-12 06:47:57 +0200

    Adds apicula toolchain to gowin platform

---
+++ litex/build/gowin/apicula.py
@@ -0,0 +1,46 @@
+from litex.build.generic_platform import *
+from litex.build import tools
+from litex.build.gowin.gowin import _build_cst
+from litex.build.yosys_nextpnr_toolchain import YosysNextPNRToolchain
+
+class GowinApiculaToolchain(YosysNextPNRToolchain):
+    family     = "gowin"
+    synth_fmt  = "json"
+    pnr_fmt    = "report"
+    packer_cmd = "gowin_pack"
+
+    def __init__(self):
+        super().__init__()
+        self.additional_cst_commands = []
+
+    def build_timing_constraints(self, vns):
+        # nextpnr-himbaechel for gowin currently doesnt supports timing constaints
+        pass
+
+    def build_io_constraints(self):
+        _build_cst(self.named_sc, self.named_pc, self.additional_cst_commands, self._build_name)
+        return (self._build_name + ".cst", "CST")
+
+    def finalize(self):
+        pnr_opts = "--write {top}_routed.json --top {top} --device {device}" + \
+            " --vopt family={devicename} --vopt cst={top}.cst"
+        self._pnr_opts += pnr_opts.format(
+            top        = self._build_name,
+            device     = self.platform.device,
+            devicename = self.platform.devicename
+        )
+
+        self._packer_opts += "-d {devicename} -o {top}.fs {top}_routed.json".format(
+            devicename = self.platform.devicename,
+            top        = self._build_name
+        )
+
+        YosysNextPNRToolchain.finalize(self)
+
+        # family is gowin but NextPNRWrapper needs to call 'nextpnr-himbaechel' not 'nextpnr-gowin'
+        self._nextpnr.name = "nextpnr-himbaechel"
+
+    def build(self, platform, fragment, **kwargs):
+        self.platform = platform
+
+        return YosysNextPNRToolchain.build(self, platform, fragment, **kwargs)
