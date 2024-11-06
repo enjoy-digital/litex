935326e66cef72381862c66241a7cb88a3f397b5
Long Pham <long_t_pham@apple.com>
2024-11-06 11:25:54 -0800

    Add FTDI serial number option to openfpgaloader, useful when multiple similar boards are connected for CI/CD

--- litex/build/openfpgaloader.py
+++ litex/build/openfpgaloader.py
@@ -12,7 +12,7 @@ from litex.build.generic_programmer import GenericProgrammer
 class OpenFPGALoader(GenericProgrammer):
     needs_bitreverse = False
 
-    def __init__(self, board="", cable="", freq=0, fpga_part="", index_chain=None):
+    def __init__(self, board="", cable="", freq=0, fpga_part="", index_chain=None, ftdi_serial=None):
         # openFPGALoader base command.
         self.cmd = ["openFPGALoader"]
 
@@ -35,6 +35,9 @@ class OpenFPGALoader(GenericProgrammer):
         # Specify index in the JTAG chain.
         if index_chain is not None:
             self.cmd += ["--index-chain", str(int(index_chain))]
+
+        if ftdi_serial is not None:
+            self.cmd += ["--ftdi-serial", str(ftdi_serial)]
 
     def load_bitstream(self, bitstream_file):
         # Load base command.
