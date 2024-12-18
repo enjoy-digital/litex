40795a2138639afb4bf71f61ed3f1bcb065cdd78
Jiaxun Yang <jiaxun.yang@flygoat.com>
2024-12-18 19:19:41 +0000

    treewide: Fix "invalid escape sequence" warnings
    Exposed with new tests.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- litex/soc/cores/cpu/openc906/core.py
+++ litex/soc/cores/cpu/openc906/core.py
@@ -61,9 +61,9 @@ def add_manifest_sources(platform, manifest):
     basedir = os.path.join(os.environ["OPENC906_DIR"], "C906_RTL_FACTORY")
     with open(os.path.join(basedir, manifest), 'r') as f:
         for l in f:
-            res = re.search('\$\{CODE_BASE_PATH\}/(.+)', l)
-            if res and not re.match('//', l):
-                if re.match('\+incdir\+', l):
+            res = re.search(r'\$\{CODE_BASE_PATH\}/(.+)', l)
+            if res and not re.match(r'//', l):
+                if re.match(r'\+incdir\+', l):
                     platform.add_verilog_include_path(os.path.join(basedir, res.group(1)))
                 else:
                     platform.add_source(os.path.join(basedir, res.group(1)))
