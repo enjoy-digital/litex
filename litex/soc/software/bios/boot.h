022b9e17f63d9fa4cd4bdc5465f03db0fe8bcb1c
Michael Betz <michibetz@gmail.com>
2025-07-19 15:06:22 +0200

    bios, main.c: reply to ping requests when IDLE

--- litex/soc/software/bios/boot.h
+++ litex/soc/software/bios/boot.h
@@ -1,9 +1,13 @@
 #ifndef __BOOT_H
 #define __BOOT_H
 
+// Set local, remote IP and MAC-address
 void set_local_ip(const char * ip_address);
 void set_remote_ip(const char * ip_address);
 void set_mac_addr(const char * mac_address);
+
+// Apply local IP and MAC-address to UDP driver
+void net_init(void);
 
 void __attribute__((noreturn)) boot(unsigned long r1, unsigned long r2, unsigned long r3, unsigned long addr);
 int serialboot(void);
@@ -12,6 +16,4 @@ void flashboot(void);
 void romboot(void);
 void sdcardboot(void);
 void sataboot(void);
-
-void net_init(void);
 #endif /* __BOOT_H */
