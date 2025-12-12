enjoy-digital/litex

commit 195cc915ed3cc6460838c2e484b836ac1e710e03

cores/cpu/vexriscv_smp: define SYNTHESIS in Quartus

litex/soc/cores/cpu/vexriscv_smp/core.py
@@ -369,6 +369,10 @@ class VexRiscvSMP(CPU):
         from litex.build.altera import AlteraPlatform
         if isinstance(platform, AlteraPlatform):
             ram_filename = "Ram_1w_1rs_Intel.v"
+            # define SYNTHESIS verilog name to avoid issues with unsupported
+            # functions
+            platform.toolchain.additional_qsf_commands.append(
+                'set_global_assignment -name VERILOG_MACRO "SYNTHESIS=1"')
         # On Efinix platforms, use specific implementation.
         from litex.build.efinix import EfinixPlatform
         if isinstance(platform, EfinixPlatform):

commit 9dd426a8a38d69899170b648d61ba8ab1012b736

build/altera/quartus: define SYNTHESIS macro globally
Fixes: 195cc915ed ("cores/cpu/vexriscv_smp: define SYNTHESIS in Quartus")
Signed-off-by: Gabriel Somlo <gsomlo@gmail.com>

litex/build/altera/quartus.py
@@ -182,6 +182,9 @@ class AlteraQuartusToolchain(GenericToolchain):
         # Set timing constraints
         qsf.append("set_global_assignment -name SDC_FILE {}.sdc".format(self._build_name))
 
+        # Define SYNTHESIS macro
+        qsf.append('set_global_assignment -name VERILOG_MACRO "SYNTHESIS=1"')
+
         # Add additional commands
         qsf += self.additional_qsf_commands
 

litex/soc/cores/cpu/vexriscv_smp/core.py
@@ -445,10 +445,6 @@ class VexRiscvSMP(CPU):
         from litex.build.altera import AlteraPlatform
         if isinstance(platform, AlteraPlatform):
             ram_filename = "Ram_1w_1rs_Intel.v"
-            # define SYNTHESIS verilog name to avoid issues with unsupported
-            # functions
-            platform.toolchain.additional_qsf_commands.append(
-                'set_global_assignment -name VERILOG_MACRO "SYNTHESIS=1"')
         # On Efinix platforms, use specific implementation.
         from litex.build.efinix import EfinixPlatform
         if isinstance(platform, EfinixPlatform):