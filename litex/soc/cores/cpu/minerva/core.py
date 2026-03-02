891eb295412afb24362b50d8836f51d2bf51f468
Florent Kermarrec <florent@enjoy-digital.fr>
2026-03-02 17:38:28 +0100

    soc/cpu: switch minerva/sentinel to Amaranth2VConverter

--- litex/soc/cores/cpu/minerva/core.py
+++ litex/soc/cores/cpu/minerva/core.py
@@ -5,16 +5,12 @@
 # Copyright (c) 2018-2019 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
-import os
-from pathlib import Path
-import shutil
-import subprocess
-
 from migen import *
 
 from litex.gen import *
 
-from litex import get_data_mod
+from litex.build.amaranth2v_converter import Amaranth2VConverter
+from litex.soc.cores.cpu.amaranth import check_required_modules, import_from_pythondata
 from litex.soc.interconnect import wishbone
 from litex.soc.cores.cpu import CPU, CPU_GCC_TRIPLE_RISCV32
 
@@ -50,88 +46,67 @@ class Minerva(CPU):
         self.variant      = variant
         self.reset        = Signal()
         self.interrupt    = Signal(16)
-        self.ibus         = ibus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
-        self.dbus         = dbus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
+        self.ibus         = wishbone.Interface(data_width=32, address_width=32, addressing="word")
+        self.dbus         = wishbone.Interface(data_width=32, address_width=32, addressing="word")
         self.periph_buses = [self.ibus, self.dbus] # Peripheral buses (Connected to main SoC's bus).
         self.memory_buses = []                     # Memory buses (Connected directly to LiteDRAM).
-
-        # # #
-
-        self.cpu_params = dict(
-            # Clk / Rst.
-            i_clk = ClockSignal("sys"),
-            i_rst = ResetSignal("sys") | self.reset,
-
-            # IRQ.
-            i_timer_interrupt    = 0,
-            i_software_interrupt = 0,
-            i_external_interrupt = 0,
-            i_fast_interrupt = self.interrupt,
-
-            # Ibus.
-            o_ibus__stb   = ibus.stb,
-            o_ibus__cyc   = ibus.cyc,
-            o_ibus__cti   = ibus.cti,
-            o_ibus__bte   = ibus.bte,
-            o_ibus__we    = ibus.we,
-            o_ibus__adr   = ibus.adr,
-            o_ibus__dat_w = ibus.dat_w,
-            o_ibus__sel   = ibus.sel,
-            i_ibus__ack   = ibus.ack,
-            i_ibus__err   = ibus.err,
-            i_ibus__dat_r = ibus.dat_r,
-
-            # Dbus.
-            o_dbus__stb   = dbus.stb,
-            o_dbus__cyc   = dbus.cyc,
-            o_dbus__cti   = dbus.cti,
-            o_dbus__bte   = dbus.bte,
-            o_dbus__we    = dbus.we,
-            o_dbus__adr   = dbus.adr,
-            o_dbus__dat_w = dbus.dat_w,
-            o_dbus__sel   = dbus.sel,
-            i_dbus__ack   = dbus.ack,
-            i_dbus__err   = dbus.err,
-            i_dbus__dat_r = dbus.dat_r,
-        )
 
     def set_reset_address(self, reset_address):
         self.reset_address = reset_address
 
-    @staticmethod
-    def elaborate(reset_address, with_icache, with_dcache, with_muldiv, verilog_filename):
-        pipx_or_pdm = shutil.which("pipx") or shutil.which("pdm")
-
-        if not pipx_or_pdm:
-            raise OSError("Unable to elaborate Minerva CPU. Make sure \"pipx\" or \"pdm\" is installed.")
-
-        cli_params = []
-        cli_params.append("--reset-addr={}".format(reset_address))
-        if with_icache:
-            cli_params.append("--with-icache")
-        if with_dcache:
-            cli_params.append("--with-dcache")
-        if with_muldiv:
-            cli_params.append("--with-muldiv")
-        cli_params.append("generate")
-        cli_params.append("--type=v")
-
-        this_dir = Path(__file__).resolve().parent
-        sdir = get_data_mod("cpu", "minerva").data_location
-
-        if subprocess.call([pipx_or_pdm, "run", this_dir / "minerva-pep-723.py", *cli_params],
-                            stdout=open(verilog_filename, "w"),
-                            cwd=sdir):
-            raise OSError("Unable to elaborate Minerva CPU, please check your Amaranth/Yosys install")
-
     def do_finalize(self):
         assert hasattr(self, "reset_address")
-        verilog_filename = os.path.join(self.platform.output_dir, "gateware", "minerva.v")
-        self.elaborate(
-            reset_address    = self.reset_address,
-            with_icache      = True,
-            with_dcache      = True,
-            with_muldiv      = True,
-            verilog_filename = verilog_filename)
-        self.platform.add_source(verilog_filename)
-        self.specials += Instance("minerva_cpu", **self.cpu_params)
+        check_required_modules({
+            "amaranth": "pip3 install --user amaranth==0.5.8",
+            "amaranth_soc": "pip3 install --user git+https://github.com/amaranth-lang/amaranth-soc.git",
+        })
+
+        minerva_core = import_from_pythondata("cpu", "minerva", "minerva.core")
+        amaranth_cpu = minerva_core.Minerva(
+            reset_address = self.reset_address,
+            with_icache   = True,
+            with_dcache   = True,
+            with_muldiv   = True,
+        )
+
+        self.converter = Amaranth2VConverter(self.platform,
+            name        = "minerva_cpu",
+            module      = amaranth_cpu,
+            core_params = dict(
+                # Clk / Rst.
+                i_sync_clk = ClockSignal("sys"),
+                i_sync_rst = ResetSignal("sys") | self.reset,
+
+                # IRQ.
+                i_timer_interrupt    = 0,
+                i_software_interrupt = 0,
+                i_external_interrupt = 0,
+                i_fast_interrupt     = self.interrupt,
+
+                # Ibus.
+                o_ibus_stb   = self.ibus.stb,
+                o_ibus_cyc   = self.ibus.cyc,
+                o_ibus_cti   = self.ibus.cti,
+                o_ibus_bte   = self.ibus.bte,
+                o_ibus_we    = self.ibus.we,
+                o_ibus_adr   = self.ibus.adr,
+                o_ibus_dat_w = self.ibus.dat_w,
+                o_ibus_sel   = self.ibus.sel,
+                i_ibus_ack   = self.ibus.ack,
+                i_ibus_err   = self.ibus.err,
+                i_ibus_dat_r = self.ibus.dat_r,
+
+                # Dbus.
+                o_dbus_stb   = self.dbus.stb,
+                o_dbus_cyc   = self.dbus.cyc,
+                o_dbus_cti   = self.dbus.cti,
+                o_dbus_bte   = self.dbus.bte,
+                o_dbus_we    = self.dbus.we,
+                o_dbus_adr   = self.dbus.adr,
+                o_dbus_dat_w = self.dbus.dat_w,
+                o_dbus_sel   = self.dbus.sel,
+                i_dbus_ack   = self.dbus.ack,
+                i_dbus_err   = self.dbus.err,
+                i_dbus_dat_r = self.dbus.dat_r,
+            ),
+        )
