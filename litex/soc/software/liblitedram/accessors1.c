d66325f4eca21b94f7b328d6f87d372a0c2168c7
Florent Kermarrec <florent@enjoy-digital.fr>
2026-05-11 20:08:28 +0000

    software: fix LiteDRAM multi-instance accessors
    Include the generated PHY header before deciding whether accessor bodies should be built so numbered DDR PHY aliases are visible. Let generated sdramN_phy.h provide CSR function aliases for each instance and keep instance.h focused on public liblitedram symbol renaming.

--- litex/soc/software/liblitedram/accessors1.c
+++ litex/soc/software/liblitedram/accessors1.c
@@ -2,10 +2,10 @@
 
 #if defined(CSR_SDRAM1_BASE) && defined(CSR_DDRPHY1_BASE)
 #define LITEDRAM_INSTANCE          sdram1
-#define LITEDRAM_SDRAM_CSR_PREFIX  sdram1
-#define LITEDRAM_DDRPHY_CSR_PREFIX ddrphy1
-#define LITEDRAM_DDRCTRL_CSR_PREFIX ddrctrl1
 #define LITEDRAM_PHY_HEADER        <generated/sdram1_phy.h>
+#undef CSR_SDRAM_BASE
+#undef CSR_DDRPHY_BASE
+#undef CSR_DDRCTRL_BASE
 #define CSR_SDRAM_BASE             CSR_SDRAM1_BASE
 #define CSR_DDRPHY_BASE            CSR_DDRPHY1_BASE
 #include <liblitedram/instance.h>
