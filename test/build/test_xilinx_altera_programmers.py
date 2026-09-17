dc6d78b002bfbf6943a70a9c8182caf9d1c3bcac
Florent Kermarrec <florent@enjoy-digital.fr>
2026-09-17 17:04:54 +0200

    test/build: extend Xilinx programmer command coverage.

--- test/build/test_xilinx_altera_programmers.py
+++ test/build/test_xilinx_altera_programmers.py
@@ -6,7 +6,7 @@
 from unittest.mock import patch
 
 from litex.build.altera.programmer import USBBlaster
-from litex.build.xilinx.programmer import Adept, XC3SProg
+from litex.build.xilinx.programmer import Adept, FpgaProg, XC3SProg
 
 
 def test_usbblaster_programmer_command():
@@ -27,6 +27,31 @@ def test_xc3sprog_programmer_load_command():
 
     assert call.call_args_list[0].args[0] == [
         "xc3sprog", "-v", "-c", "xpc", "-p", "1", "/tmp/top.bit"
+    ]
+
+
+def test_xc3sprog_programmer_flash_command():
+    programmer = XC3SProg(cable="xpc", position=1, flash_proxy_basename="bscan_spi_xc6s")
+    with patch.object(programmer, "find_flash_proxy", return_value="bscan_spi_xc6s.bit"), \
+         patch.object(programmer, "call") as call:
+        programmer.flash(0x40000, "/tmp/top.bit")
+
+    assert call.call_args_list[0].args[0] == [
+        "xc3sprog", "-v", "-c", "xpc", "-p", "1",
+        "-Ibscan_spi_xc6s.bit", "/tmp/top.bit:w:0x40000:BIN"
+    ]
+
+
+def test_fpgaprog_programmer_load_and_flash_commands():
+    programmer = FpgaProg(flash_proxy_basename="bscan_spi_xc6s")
+    with patch.object(programmer, "find_flash_proxy", return_value="bscan_spi_xc6s.bit"), \
+         patch.object(programmer, "call") as call:
+        programmer.load_bitstream("/tmp/top.bit")
+        programmer.flash(0, "/tmp/top.bit")
+
+    assert call.call_args_list[0].args[0] == ["fpgaprog", "-v", "-f", "/tmp/top.bit"]
+    assert call.call_args_list[1].args[0] == [
+        "fpgaprog", "-v", "-sa", "-r", "-b", "bscan_spi_xc6s.bit", "-f", "/tmp/top.bit"
     ]
 
 
